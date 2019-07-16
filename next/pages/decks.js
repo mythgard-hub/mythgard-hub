48528bc4f5aa881c5fccec0cec4324f1485eb804
alexMA <almiddeleer@gmail.com>
2019-07-15 22:04:29 -0400

    Start work on deck search form

--- next/pages/decks.js
+++ next/pages/decks.js
@@ -1,11 +1,32 @@
+import React from 'react';
 import Layout from '../components/layout';
 import AllDecks from '../components/all-decks';
+import DeckSearchForm from '../components/deck-search-form';
+import PropTypes from 'prop-types';
 
-const index = () => (
-  <Layout>
-    <h1>Decks</h1>
-    <AllDecks />
-  </Layout>
-);
+class DecksPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handlSearchSubmit = this.handleSearchSubmit.bind(this);
+  }
 
-export default index;
+  handleSearchSubmit(search) {
+    this.setState({ search });
+  }
+
+  render() {
+    return (
+      <Layout>
+        <h1>Decks</h1>
+        <DeckSearchForm onSubmit={this.handleSearchSubmit.bind(this)} />
+        <AllDecks search={this.props.search} />
+      </Layout>
+    );
+  }
+}
+DecksPage.propTypes = {
+  search: PropTypes.object
+};
+
+export default DecksPage;
