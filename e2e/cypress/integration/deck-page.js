693620831b793de99a812b332485a3983651fd10
alexMA <almiddeleer@gmail.com>
2019-07-21 00:33:01 -0400

    Add card list to deck page with tests

--- e2e/cypress/integration/deck-page.js
+++ e2e/cypress/integration/deck-page.js
@@ -5,4 +5,13 @@ describe('Deck Page', function() {
   it('has a name', function() {
     cy.get('h1').should('have.class', 'deckName');
   });
+  it('should have cards', function() {
+    cy.get('.deckList').should('have.length', 1);
+  });
+  it('should link from cards to /card', function() {
+    cy.get('.deckList a:first').click();
+    cy.location().should(location => {
+      expect(location.pathname).to.contain('/card');
+    });
+  });
 });
--- next/components/deck.js
+++ next/components/deck.js
@@ -1,10 +1,49 @@
 import PropTypes from 'prop-types';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import ErrorMessage from './error-message';
+import Link from 'next/link';
+
+export const deckCardsQuery = gql`
+  query($id: Int!) {
+    deck(id: $id) {
+      id
+      name
+      cardDecks {
+        nodes {
+          card {
+            name
+            id
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default function Deck({ deck }) {
   return (
-    <div>
-      <h1 className="deckName">{deck.name}</h1>
-    </div>
+    <Query query={deckCardsQuery} variables={{ id: deck.id }}>
+      {({ loading, error, data: { deck } }) => {
+        if (error) return <ErrorMessage message="Error loading decks." />;
+        if (loading) return <div>Loading</div>;
+
+        return (
+          <>
+            <h1 className="deckName">{deck.name}</h1>
+            <ul className="deckList">
+              {deck.cardDecks.nodes.map(({ card }) => (
+                <li key={card.id}>
+                  <Link href={`/card?id=${card.id}`}>
+                    <a>{card.name}</a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </>
+        );
+      }}
+    </Query>
   );
 }
 Deck.propTypes = {
