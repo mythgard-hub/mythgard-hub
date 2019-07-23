8a8b7cc27249c2e3dba9a93939f8b9c7b8effacd
Justin Russell <jus.russell@gmail.com>
2019-07-22 23:11:01 -0400

    Add google analytics (#3)
    * Add Google Analytics in production
    
    * Don't sniff the env
    
    * Dockerfile style

--- next/pages/_app.js
+++ next/pages/_app.js
@@ -1,8 +1,13 @@
 import App, { Container } from 'next/app';
 import Head from 'next/head';
-import React from 'react';
+import Router from 'next/router';
 import withApolloClient from '../components/with-apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { pageview, USE_GOOGLE_ANALYTICS } from '../lib/gtag';
+
+if (USE_GOOGLE_ANALYTICS) {
+  Router.events.on('routeChangeComplete', url => pageview(url));
+}
 
 class MyApp extends App {
   render() {
@@ -11,7 +16,7 @@ class MyApp extends App {
       <Container>
         <ApolloProvider client={apolloClient}>
           <Head>
-            <meta charset="utf-8" />
+            <meta charSet="utf-8" />
             <meta
               name="viewport"
               key="viewport"
