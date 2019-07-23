3a3bff1227aa78515f1d3a156b808ad23259103c
jtrussell <jus.russell@gmail.com>
2019-07-22 22:23:48 -0400

    Add Google Analytics in production

--- next/pages/_app.js
+++ next/pages/_app.js
@@ -1,8 +1,19 @@
 import App, { Container } from 'next/app';
 import Head from 'next/head';
-import React from 'react';
+import Router from 'next/router';
 import withApolloClient from '../components/with-apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { pageview } from '../lib/gtag';
+
+if (process.env.NODE_ENV === 'production') {
+  /**
+   * Include GA pageview tracking in production
+   *
+   * If you change the environment check here be sure to make corresponding
+   * updates to the _document template.
+   */
+  Router.events.on('routeChangeComplete', url => pageview(url));
+}
 
 class MyApp extends App {
   render() {
