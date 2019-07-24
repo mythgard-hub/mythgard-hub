cbec9452d2cbc74c5be1d4ec10b56e8a42d7c45a
alexMA <almiddeleer@gmail.com>
2019-07-23 20:57:13 -0400

    partial

--- next/components/deck-comments.js
+++ next/components/deck-comments.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
@@ -17,26 +18,88 @@ export const deckCommentsQuery = gql`
   }
 `;
 
-export default function DeckComments({ deck }) {
-  return (
-    <Query query={deckCommentsQuery} variables={{ id: deck.id }}>
-      {({ loading, error, data: { deck } }) => {
-        if (error) return <ErrorMessage message="Error loading comments." />;
-        if (loading) return <div>Loading</div>;
+export const addCommentQuery = gql`
+  mutation CreateDeckComment($deck_id: Int!, $body: String!) {
+    createDeckComment(
+      input: { deckComment: { deckId: $deck_id, body: $body } }
+    ) {
+      deckComment {
+        body
+        id
+      }
+    }
+  }
+`;
 
-        return (
-          <>
-            <h2 className="deckCommentsHeader">Comments</h2>
-            <CommentList comments={deck.deckComments.nodes} />
-          </>
-        );
-      }}
-    </Query>
-  );
+class DeckComments extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { commentBody: '', deckId: props.deck.id };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(e, refetchFn) {
+    e.preventDefault();
+    refetchFn();
+  }
+
+  render() {
+    return (
+      <Query query={deckCommentsQuery} variables={{ id: this.props.deck.id }}>
+        {({ loading, error, data: { deck }, refetch }) => {
+          if (error) return <ErrorMessage message="Error loading comments." />;
+          if (loading) return <div>Loading</div>;
+
+          return (
+            <>
+              <h2 className="deckCommentsHeader">Comments</h2>
+              <CommentList comments={deck.deckComments.nodes} />
+              <form>
+                <label>
+                  Add a comment:
+                  <input
+                    type="text"
+                    value={this.state.commentBody}
+                    name="commentBody"
+                    className="commentBody"
+                    onChange={this.handleInputChange}
+                  />
+                </label>
+                <br />
+                <br />
+                <input
+                  type="submit"
+                  value="Submit"
+                  onClick={e => {
+                    this.handleSubmit(e, refetch);
+                  }}
+                />
+              </form>
+            </>
+          );
+        }}
+      </Query>
+    );
+  }
 }
+
 DeckComments.propTypes = {
   deck: PropTypes.shape({
     id: PropTypes.string,
     deckComments: PropTypes.array
   }).isRequired
 };
+
+export default DeckComments;
