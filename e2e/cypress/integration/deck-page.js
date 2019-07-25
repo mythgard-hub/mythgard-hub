b3c33574c5f843066906d91df7fbc4f8cd4e0b9a
alexMA <almiddeleer@gmail.com>
2019-07-24 20:04:52 -0400

    Add test for adding comments

--- e2e/cypress/integration/deck-page.js
+++ e2e/cypress/integration/deck-page.js
@@ -2,7 +2,7 @@ describe('Deck Page', function() {
   beforeEach(() => {
     cy.visit('/deck/1');
   });
-  it('has a name', function() {
+  it('has a name and cards, ', function() {
     cy.get('h1').should('have.class', 'deckName');
   });
   it('should have cards', function() {
@@ -16,5 +16,13 @@ describe('Deck Page', function() {
   });
   it('should have comments', function() {
     cy.get('.commentList').should('have.length', 1);
+  });
+  it('should add a comment', async function() {
+    const list = await cy.get('.commentList li');
+    const beforeLength = list.length;
+    assert.equal(true, true);
+    cy.get('input.commentBody').type('I made masters with this');
+    cy.get('input[type="submit"]').click();
+    cy.get('.commentList li').should('have.length', beforeLength + 1);
   });
 });
