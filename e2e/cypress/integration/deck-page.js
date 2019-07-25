71f282ac221730cbc600a3d2064c34c9fed65108
alexMA <almiddeleer@gmail.com>
2019-07-24 21:02:19 -0400

    refactor

--- e2e/cypress/integration/deck-page.js
+++ e2e/cypress/integration/deck-page.js
@@ -2,22 +2,16 @@ describe('Deck Page', function() {
   beforeEach(() => {
     cy.visit('/deck/1');
   });
-  it('has a name and cards, ', function() {
+  it('happy path card click', function() {
     cy.get('h1').should('have.class', 'deckName');
-  });
-  it('should have cards', function() {
     cy.get('.cardList').should('have.length', 1);
-  });
-  it('should link from cards to /card', function() {
     cy.get('.cardList a:first').click();
     cy.location().should(location => {
       expect(location.pathname).to.contain('/card');
     });
   });
-  it('should have comments', function() {
+  it('happy path comments', async function() {
     cy.get('.commentList').should('have.length', 1);
-  });
-  it('should add a comment', async function() {
     const list = await cy.get('.commentList li');
     const beforeLength = list.length;
     assert.equal(true, true);
