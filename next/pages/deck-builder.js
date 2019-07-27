12446b3591191572d3e16a2ebb85a18697de0ed9
alexMA <almiddeleer@gmail.com>
2019-07-27 13:40:17 -0400

    Structural styles for deckbuilder

--- next/pages/deck-builder.js
+++ next/pages/deck-builder.js
@@ -2,18 +2,23 @@ import Layout from '../components/layout';
 import AllCards from '../components/all-cards';
 import CardList from '../components/card-list';
 
-const pageStyles = `
-  .deck-builder-panels {
-    color: red;
-  }
-`;
-
 export default () => (
   <Layout>
-    <style jsx>{pageStyles}</style>
+    <style jsx>{`
+      .deck-builder-panels {
+        display: flex;
+        align-items: flex-start;
+      }
+      .collection {
+        flex-grow: 1;
+      }
+    `}</style>
     <h1 data-cy="header">Deck Builder</h1>
     <div className="deck-builder-panels">
-      <AllCards />
+      <div className="collection">
+        <h2>Collection</h2>
+        <AllCards />
+      </div>
       <div className="deck-in-progress" data-cy="deckInProgress">
         <h2>Current Deck</h2>
         <CardList cards={[]} />
