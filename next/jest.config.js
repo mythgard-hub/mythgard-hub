6056a5fb88206bca7af8abbd513cc052597a5d30
Lily Pytel <lily.pytel@gmail.com>
2019-07-27 22:00:40 -0400

    Fix jest and enzyme and babel config

--- next/jest.config.js
+++ next/jest.config.js
@@ -4,6 +4,9 @@ module.exports = {
   coveragePathIgnorePatterns: ['/node_modules/', 'enzyme.js'],
   setupTestFrameworkScriptFile: '<rootDir>/enzyme.js',
   coverageReporters: ['json', 'lcov', 'text', 'text-summary'],
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest'
+  },
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/mocks.js',
