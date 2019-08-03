810d1e528790845f8bc434c28c7fbe7b25e80631
alexMA <almiddeleer@gmail.com>
2019-08-02 22:55:02 -0400

    Test for card page

--- next/components/card.js
+++ next/components/card.js
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types';
 export default function Card({ card }) {
   return (
     <div>
-      <h1 className="cardName">{card.name}</h1>
-      <div>rules: {card.rules}</div>
+      <h1 data-cy="cardName" className="cardName">
+        {card.name}
+      </h1>
+      <div data-cy="cardRules">rules: {card.rules}</div>
     </div>
   );
 }
