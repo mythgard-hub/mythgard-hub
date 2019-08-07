08a46b1dcd787d81051ce5f884c028a9f47e012c
Lily Pytel <lily.pytel@gmail.com>
2019-08-06 23:18:57 -0400

    Create an export utility function to integrate in the deck builder

---
+++ next/lib/export-utils.js
@@ -0,0 +1,20 @@
+export const exportDeck = deckInProgress => {
+  try {
+    const exportMeta = [
+      `name: ${deckInProgress.deckName}`,
+      `path: ${deckInProgress.deckPath}`,
+      `power: ${deckInProgress.deckPower}`,
+      `coverart: ${deckInProgress.deckCoverArt}`
+    ].join('\n');
+
+    const cards = Object.values(deckInProgress.mainDeck)
+      .map(c => `${c.quantity} ${c.name}`)
+      .join('\n');
+
+    const all = [exportMeta, cards];
+
+    return all.join('\n');
+  } catch (e) {
+    return '';
+  }
+};
