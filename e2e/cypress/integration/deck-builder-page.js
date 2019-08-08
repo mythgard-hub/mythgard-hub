f342a97899834231d0a25dc5e9c8d3fc8c814a95
Lily Pytel <lily.pytel@gmail.com>
2019-08-07 20:40:08 -0400

    Add happy path tests for import and export

--- e2e/cypress/integration/deck-builder-page.js
+++ e2e/cypress/integration/deck-builder-page.js
@@ -6,6 +6,10 @@ describe('Deck builder page', () => {
     cy.get('[data-cy="header"]').should('be.visible');
     cy.get('[data-cy="cardList"]').should('be.visible');
     cy.get('[data-cy="deckInProgress"]').should('be.visible');
+    cy.get('[data-cy="importDeckTitle"]').should('be.visible');
+    cy.get('[data-cy="importDeckTextarea"]').should('be.visible');
+    cy.get('[data-cy="importDeckButton"]').should('be.visible');
+
     cy.get('[data-cy="cardListCard"]:first').click();
     cy.get('[data-cy="deckInProgress"]').should('be.visible');
     cy.get('[data-cy="deckInProgress"] [data-cy="cardListCard"]').should(
@@ -17,5 +21,39 @@ describe('Deck builder page', () => {
     cy.location().should(location => {
       expect(location.pathname).to.eq('/deck');
     });
+  });
+
+  it('should import a deck', function() {
+    const input = [
+      'name: my deck',
+      'path: my path',
+      'power: my power',
+      'coverart: myself',
+      '1 Dragon',
+      '2 Imp'
+    ].join('\n');
+
+    cy.get('[data-cy="importDeckTextarea"]').type(input);
+    cy.get('[data-cy="importDeckButton"]').click();
+
+    cy.get('[data-cy="deckTitle"]').should('have.value', 'my deck');
+    cy.get('[data-cy="cardListCard"').should('have.length', 6);
+  });
+
+  it('should export a deck', function() {
+    const input = [
+      'name: my deck',
+      'path: my path',
+      'power: my power',
+      'coverart: myself',
+      '1 Dragon',
+      '2 Imp'
+    ].join('\n');
+
+    cy.get('[data-cy="importDeckTextarea"]').type(input);
+    cy.get('[data-cy="importDeckButton"]').click();
+    cy.get('[data-cy="exportDeckButton"]').click();
+
+    cy.get('[data-cy="exportDeckSuccess"]').contains('Copied');
   });
 });
