ea1d7f15f571f5911a7f805d87d5264cbd37ca12
alexMA <almiddeleer@gmail.com>
2019-08-10 13:08:01 -0400

    Fix tests

--- e2e/cypress/integration/deck-builder-page.js
+++ e2e/cypress/integration/deck-builder-page.js
@@ -58,7 +58,7 @@ describe('Deck builder page', () => {
     cy.get('[data-cy="importDeckButton"]').click();
 
     cy.get('[data-cy="deckTitle"]').should('have.value', 'my deck');
-    cy.get('[data-cy="cardListCard"').should('have.length', 6);
+    cy.get(`${deckInProgress} ${cardListCard}`).should('have.length', 2);
   });
 
   it('should export a deck', function() {
