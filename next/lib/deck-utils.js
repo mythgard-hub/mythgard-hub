bf447bc2f4d0aa2d0f50f40eaca68d9ebb741090
Lily Pytel <lily.pytel@gmail.com>
2019-08-11 00:31:56 -0400

    Add a helper method and test to add a card to a deck with quantities

--- next/lib/deck-utils.js
+++ next/lib/deck-utils.js
@@ -9,3 +9,21 @@ export const initializeDeckBuilder = () => {
     errors: []
   };
 };
+
+export const addCardToDeck = (deck, card) => {
+  const nextDeck = { ...deck };
+
+  if (!nextDeck.hasOwnProperty(card.card.id)) {
+    nextDeck[card.card.id] = card;
+    return nextDeck;
+  }
+
+  const oldQuantity = nextDeck[card.card.id].quantity;
+
+  nextDeck[card.card.id] = {
+    ...nextDeck[card.card.id],
+    quantity: oldQuantity + card.quantity
+  };
+
+  return nextDeck;
+};
