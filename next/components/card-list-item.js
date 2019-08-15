543d5c29218a1fed3feb9055449ff3f2a208d2c2
alexMA <almiddeleer@gmail.com>
2019-08-14 22:31:47 -0400

    Fix a11y-unfriendly buttons

--- next/components/card-list-item.js
+++ next/components/card-list-item.js
@@ -4,11 +4,20 @@ import PropTypes from 'prop-types';
 
 export default function CardListItem({ card, onClick }) {
   return (
-    <Link href={`/card?id=${card.id}`}>
-      <a data-cy="cardListCard" onClick={e => onClick(e, card)}>
-        {card.id}. name: {card.name}
-      </a>
-    </Link>
+    <>
+      {onClick && (
+        <span data-cy="cardListCard" onClick={e => onClick(e, card)}>
+          {card.id}. name: {card.name}
+        </span>
+      )}
+      {!onClick && (
+        <Link href={`/card?id=${card.id}`}>
+          <a data-cy="cardListCard">
+            {card.id}. name: {card.name}
+          </a>
+        </Link>
+      )}
+    </>
   );
 }
 
@@ -18,8 +27,4 @@ CardListItem.propTypes = {
     name: PropTypes.string.isRequired
   }).isRequired,
   onClick: PropTypes.func
-};
-
-CardListItem.defaultProps = {
-  onClick: () => {}
 };
