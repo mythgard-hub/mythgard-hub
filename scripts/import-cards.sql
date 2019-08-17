31f4fb5b90db941d6c672da8b62ab7034fc42863
alexMA <almiddeleer@gmail.com>
2019-08-16 22:27:45 -0400

    rename stuffs

--- scripts/import-cards.sql
+++ scripts/import-cards.sql
@@ -10,14 +10,21 @@ CREATE TEMPORARY TABLE t (
   rarity VARCHAR(255),
   atk VARCHAR(255),
   def VARCHAR(255),
-  text VARCHAR(255),
+  rules TEXT,
   flavor VARCHAR(255),
   set INTEGER,
   owned BOOLEAN);
 
+\copy t(id, name, facOne, facTwo, type, subtype, manaCost, gemCost, rarity, atk, def, rules, flavor, set, owned) FROM 'mgcards.csv' WITH CSV HEADER;
 
-
-\copy t(id, name, facOne, facTwo, type, subtype, manaCost, gemCost, rarity, atk, def, text, flavor, set, owned) FROM 'mgcards.csv' WITH CSV HEADER;
+insert into mythgard.card (name, rules, type, atk, def)
+select
+  name,
+  rules,
+  type,
+  REGEXP_REPLACE(atk, '[^0-9]' ,'-1')::integer,
+  REGEXP_REPLACE(def, '[^0-9]' ,'-1')::integer
+  from t;
 
 DROP TABLE t;
 -- \copy mythgard.card(name, rules) To 'mgcards.csv' With CSV HEADER;
