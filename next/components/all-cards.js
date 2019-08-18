5f66cbea2f0c815293312a69b6b88c5c98b6e55c
Lily Pytel <lily.pytel@gmail.com>
2019-08-18 19:04:59 -0400

    Remove query component from all-cards

--- next/components/all-cards.js
+++ next/components/all-cards.js
@@ -1,4 +1,4 @@
-import { Query } from 'react-apollo';
+import { useQuery } from 'react-apollo-hooks';
 import ErrorMessage from './error-message';
 import CardList from './card-list';
 import PropTypes from 'prop-types';
@@ -6,18 +6,15 @@ import PropTypes from 'prop-types';
 import allCardsQuery from '../lib/queries/all-cards-query';
 
 export default function AllCards(props) {
-  return (
-    <Query query={allCardsQuery}>
-      {({ loading, error, data }) => {
-        if (error) return <ErrorMessage message={error} />;
-        if (loading) return <div>Loading</div>;
+  const [loading, error, data] = useQuery(allCardsQuery);
 
-        const cards = data.cards;
-        return <CardList onCardClick={props.onCardClick} cards={cards.nodes} />;
-      }}
-    </Query>
-  );
+  if (error) return <ErrorMessage message={error} />;
+  if (loading) return <div>Loading cards...</div>;
+
+  const cards = data.cards;
+  return <CardList onCardClick={props.onCardClick} cards={cards.nodes} />;
 }
+
 AllCards.propTypes = {
   onCardClick: PropTypes.func
 };
