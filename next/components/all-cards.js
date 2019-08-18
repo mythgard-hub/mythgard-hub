995e780d87ca044712cf35205f8e0e926003f475
Lily Pytel <lily.pytel@gmail.com>
2019-08-18 18:57:53 -0400

    Little error checking for graphql

--- next/components/all-cards.js
+++ next/components/all-cards.js
@@ -8,10 +8,11 @@ import allCardsQuery from '../lib/queries/all-cards-query';
 export default function AllCards(props) {
   return (
     <Query query={allCardsQuery}>
-      {({ loading, error, data: { cards } }) => {
+      {({ loading, error, data }) => {
         if (error) return <ErrorMessage message={error} />;
         if (loading) return <div>Loading</div>;
 
+        const cards = data.cards;
         return <CardList onCardClick={props.onCardClick} cards={cards.nodes} />;
       }}
     </Query>
