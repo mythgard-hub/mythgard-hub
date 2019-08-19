e07439654dc5da0a17e65ffe889d7b04315cb492
Lily Pytel <lily.pytel@gmail.com>
2019-08-19 06:00:53 -0400

    Fix typo in init db

--- postgres/init.sql
+++ postgres/init.sql
@@ -32,7 +32,7 @@ CREATE TABLE mythgard.path (
 
 INSERT INTO mythgard.path ("id", "name") VALUES (1, 'Way of the Black Lotus');
 INSERT INTO mythgard.path ("id", "name") VALUES (2, 'Path to Redemption');
-INSERT INTO mythgard.path ("id", "name") VALUES (1, 'Path Variable');
+INSERT INTO mythgard.path ("id", "name") VALUES (3, 'Path Variable');
 
 CREATE TABLE mythgard.power (
   id SERIAL PRIMARY KEY,
@@ -40,8 +40,8 @@ CREATE TABLE mythgard.power (
 );
 
 INSERT INTO mythgard.power ("id", "name") VALUES (1, 'It''s over 9000!!');
-INSERT INTO mythgard.power ("id", "name") VALUES (1, 'Power Rangers');
-INSERT INTO mythgard.power ("id", "name") VALUES (1, 'Powerpuff Girls');
+INSERT INTO mythgard.power ("id", "name") VALUES (2, 'Power Rangers');
+INSERT INTO mythgard.power ("id", "name") VALUES (3, 'Powerpuff Girls');
 
 CREATE TABLE mythgard.deck (
   id SERIAL PRIMARY KEY,
