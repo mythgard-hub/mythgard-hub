1f8f9323e1553dcca2b86b788e7a2f1a2f446e6b
Lily Pytel <lily.pytel@gmail.com>
2019-08-18 21:36:52 -0400

    Add styling for deck-card-table

--- next/components/deck-card-table.js
+++ next/components/deck-card-table.js
@@ -1,30 +1,45 @@
 import PropTypes from 'prop-types';
 
-export default function DeckCardsTable({ deckCards }) {
+export default function DeckCardsTable({ deck }) {
+  const deckCards = deck && Object.values(deck.mainDeck);
+
   const drawGems = gem => {
     return Array(gem)
       .fill(0)
-      .map((_, i) => <span key={i} />);
+      .map((_, i) => <span key={i} className="gem-circle" />);
   };
 
   return (
-    <div>
+    <div className="deck-card-table-container">
       <style jsx>{`
-        .gem-circle {
-          content: ' ';
-          height: 5px;
-          width: 5px;
-          background-color: #bbb;
-          border-radius: 50%;
-          display: inline-block;
+        .deck-card-table-container {
+          margin-top: 20px;
+          padding: 10px;
         }
-
-        .deck-card-table td {
+        .deck-card-table {
+          border-collapse: collapse;
+          width: 100%;
+        }
+        td {
+          padding: 2px 5px 5px 5px;
           border: 1px solid #03080a;
         }
+        .deck-title {
+          font-size: 30px;
+          margin: 0 0 20px 5px;
+        }
       `}</style>
-      <table>
+      <div className="deck-title">{deck.deckName}</div>
+      <table className="deck-card-table">
         <tbody>
+          <tr>
+            <td colSpan={2}>Path</td>
+            <td colSpan={3}>{deck.deckPath}</td>
+          </tr>
+          <tr>
+            <td colSpan={2}>Power</td>
+            <td colSpan={3}>{deck.deckPower}</td>
+          </tr>
           {deckCards.map(deckCard => (
             <tr key={deckCard.card.id}>
               <td>{deckCard.card.mana}</td>
@@ -41,16 +56,21 @@ export default function DeckCardsTable({ deckCards }) {
 }
 
 DeckCardsTable.propTypes = {
-  deckCards: PropTypes.arrayOf(
-    PropTypes.shape({
-      quantity: PropTypes.number.isRequired,
+  deck: PropTypes.shape({
+    deckName: PropTypes.string,
+    deckPath: PropTypes.string,
+    deckPower: PropTypes.string,
+    deckCoverArt: PropTypes.string,
+    mainDeck: PropTypes.shape({
+      quantity: PropTypes.number,
       card: PropTypes.shape({
         id: PropTypes.number.isRequired,
         name: PropTypes.string.isRequired,
         mana: PropTypes.number,
         gem: PropTypes.number,
         color: PropTypes.string
-      }).isRequired
-    })
-  ).isRequired
+      })
+    }),
+    errors: PropTypes.arrayOf(PropTypes.string)
+  })
 };
