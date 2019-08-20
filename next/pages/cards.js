ab73cdcb1ea2ae1c98d4ab5d96dda74e8998853c
alexMA <almiddeleer@gmail.com>
2019-08-19 21:49:56 -0400

    Update cards page to be a class component

--- next/pages/cards.js
+++ next/pages/cards.js
@@ -1,14 +1,38 @@
 import AllCards from '../components/all-cards';
+import SomeCards from '../components/some-cards.js';
 import Layout from '../components/layout';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const index = () => (
-  <Layout
-    title="Mythgard Hub | Cards"
-    desc="Browse and search for Mythgard cards"
-  >
-    <h1>Cards</h1>
-    <AllCards />
-  </Layout>
-);
+const hasSearch = () => true;
 
-export default index;
+class CardsPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Layout
+        title="Mythgard Hub | Cards"
+        desc="Browse and search for Mythgard cards"
+      >
+        <h1>Cards</h1>
+        {hasSearch(this.state.searchQuery) ? (
+          <div>some cards</div>
+        ) : (
+          // <SomeCards search={this.state.searchQuery} />
+          <AllCards />
+        )}
+      </Layout>
+    );
+  }
+}
+
+CardsPage.propTypes = {
+  // children: PropTypes.node,
+  // className: PropTypes.string,
+};
+
+export default CardsPage;
