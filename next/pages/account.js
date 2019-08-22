9611a3a422d9faac6862f3f9f383b5018636829b
Evan Sheffield <evan.c.sheffield@gmail.com>
2019-08-22 18:33:38 -0400

    partial: Building out profile page

--- next/pages/account.js
+++ next/pages/account.js
@@ -1,26 +1,69 @@
 import React from 'react';
 import Layout from '../components/layout';
-import UserContext from '../components/user-context'
+import UserContext from '../components/user-context';
 
 class Account extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      username: context.user.username
+    };
+  }
+
   static get requiresAuth() {
     return true;
   }
 
   render() {
     const { user } = this.context;
+    const { username } = this.state;
     return (
       <Layout
         title="Mythgard Hub | Account Settings"
         desc="The Mythgard Hub privacy policy"
       >
-        <h1>My Account</h1>
-        {user && `Account Things for ${user.email}`}
+        <style jsx>{`
+          .stack {
+            --space: 1.5rem;
+          }
+
+          .stack > * {
+            margin-top: 0;
+            margin-bottom: 0;
+          }
+
+          .stack > * + * {
+            margin-top: var(--space);
+          }
+        `}</style>
+        <h1>My Profile</h1>
+
+        <h2>Settings</h2>
+
+        <div className="stack">
+          <div>
+            <label>Email: </label>
+            <span>{user.email}</span>
+          </div>
+          <div>
+            <label>User Name: </label>
+            <input
+              data-cy="deckTitle"
+              type="text"
+              name="deckName"
+              onChange={e => {
+                this.setState({ username: e.target.value });
+              }}
+              value={username}
+            />
+          </div>
+        </div>
       </Layout>
     );
   }
 }
 
-Account.contextType = UserContext
+Account.contextType = UserContext;
 
 export default Account;
