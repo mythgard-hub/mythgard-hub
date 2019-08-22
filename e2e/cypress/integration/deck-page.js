c2b16ae366aa3ca14a6269606436c7ed468762c6
alexMA <almiddeleer@gmail.com>
2019-08-22 19:16:45 -0400

    Delete comments feature.
    We may bring this back if there is sufficient demand

--- e2e/cypress/integration/deck-page.js
+++ e2e/cypress/integration/deck-page.js
@@ -9,14 +9,4 @@ describe('Deck Page', function() {
       expect(location.pathname).to.contain('/card');
     });
   });
-  it('happy path comments', function() {
-    const commentSelector = '[data-cy=commentList] [data-cy=comment]';
-    cy.get('[data-cy=commentList]').should('have.length', 1);
-    cy.get(commentSelector).then(list => {
-      cy.get('[data-cy=commentBody]').type('I made masters with this');
-      cy.get('[data-cy=submit]').click();
-      cy.get(commentSelector).should('have.length', list.length + 1);
-      cy.get('[data-cy=commentBody]').should('have.value', '');
-    });
-  });
 });
