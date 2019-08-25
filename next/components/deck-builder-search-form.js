8fb3de7f8501339835ceda9a225829661307cd90
alexMA <almiddeleer@gmail.com>
2019-08-25 12:04:03 -0400

    Remove unused

--- next/components/deck-builder-search-form.js
+++ next/components/deck-builder-search-form.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import CardSearch from './card-search.js';
 import { handleInputChangeStateless } from '../lib/form-utils.js';
