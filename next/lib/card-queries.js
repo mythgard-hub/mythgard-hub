5638c95ad9d12661472663f3d1882d83bc2f1c98
alexMA <almiddeleer@gmail.com>
2019-08-26 19:47:07 -0400

    refactor

--- next/lib/card-queries.js
+++ next/lib/card-queries.js
@@ -1,48 +1,23 @@
 import gql from 'graphql-tag';
 import { useQuery } from 'react-apollo-hooks';
 
-export const getFactionsFilter = () => {
-  return `
-    cardFactions: {
-      some: {
-        faction: {
-          name: {
-            in: $factionIds
-          }
-        }
-      }
-    }
-  `;
-};
-
-export const getTextContainsFilter = () => {
-  return `
-    or: [{
-      name: {
-        includesInsensitive: $searchText
-      }
-    },{
-      rules: {
-        includesInsensitive: $searchText
-      }
-    }]
-  `;
-};
-
-export const getRarityFilter = () => {
-  return `
-    rarity: {
-      in: $rarities
-    }
-  `;
-};
-
-export const getCardsQuery = filters => {
+export const getCardsQuery = () => {
   return gql`
-    query cards($searchText: String, $rarities: [Rarity!], $factionIds: [String!]){
-      cards(filter: {
-        ${filters}
-      }){
+    query cards(
+      $searchText: String
+      $rarities: [Rarity!]
+      $factionIds: [String!]
+    ) {
+      cards(
+        filter: {
+          or: [
+            { name: { includesInsensitive: $searchText } }
+            { rules: { includesInsensitive: $searchText } }
+          ]
+          rarity: { in: $rarities }
+          cardFactions: { some: { faction: { name: { in: $factionIds } } } }
+        }
+      ) {
         nodes {
           name
           id
@@ -52,16 +27,8 @@ export const getCardsQuery = filters => {
   `;
 };
 
-const getFilters = () => {
-  const queryFilters = [];
-  queryFilters.push(getFactionsFilter());
-  queryFilters.push(getTextContainsFilter());
-  queryFilters.push(getRarityFilter());
-  return queryFilters;
-};
-
 export const executeCardQuery = (factions, text, rarities) => {
-  const query = getCardsQuery(getFilters());
+  const query = getCardsQuery();
   return useQuery(query, {
     variables: {
       searchText: text || null,
