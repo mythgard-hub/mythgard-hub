06dd8415cc0b315b93bd6d138ca31537c698434c
Lily Pytel <lily.pytel@gmail.com>
2019-08-29 06:41:33 -0400

    Make the card search form stateless

--- next/components/card-search-form.js
+++ next/components/card-search-form.js
@@ -1,54 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { handleInputChange } from '../lib/form-utils.js';
+import { handleInputChangeStateless } from '../lib/form-utils.js';
 import FactionFilters from './faction-filters.js';
 
-class CardSearchForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { text: '' };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleInputChange = handleInputChange.bind(this);
-    this.onFactionClick = this.onFactionClick.bind(this);
-  }
+export default function CardSearchForm(props) {
+  const { onSubmit } = props;
+  const [text, setText] = useState('');
+  const [factions, setFactions] = useState(null);
 
-  handleSubmit(e) {
+  const handleSubmit = e => {
     e && e.preventDefault();
-    this.props.onSubmit(this.state);
-  }
+    onSubmit({ text, factions });
+  };
 
-  onFactionClick(factions) {
-    this.setState({ factions });
-  }
-
-  render() {
-    return (
-      <>
-        <input
-          type="text"
-          value={this.state.text}
-          name="text"
-          placeholder="Name or Rules Text"
-          maxLength="100"
-          data-cy="cardSearchText"
-          onChange={this.handleInputChange}
-        />
-        <br />
-        <br />
-        <FactionFilters onFactionClick={this.onFactionClick} />
-        <input
-          data-cy="cardSearchSubmit"
-          type="submit"
-          value="Search"
-          onClick={this.handleSubmit}
-        />
-      </>
-    );
-  }
+  return (
+    <>
+      <input
+        type="text"
+        value={text}
+        name="text"
+        placeholder="Name or Rules Text"
+        maxLength="100"
+        data-cy="cardSearchText"
+        onChange={handleInputChangeStateless(setText)}
+      />
+      <br />
+      <br />
+      <FactionFilters
+        onFactionClick={newFactions => setFactions(newFactions)}
+      />
+      <input
+        data-cy="cardSearchSubmit"
+        type="submit"
+        value="Search"
+        onClick={handleSubmit}
+      />
+    </>
+  );
 }
 
 CardSearchForm.propTypes = {
   onSubmit: PropTypes.func.isRequired
 };
-
-export default CardSearchForm;
