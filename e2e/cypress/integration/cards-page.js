197c641c06483efb130301e1d5d55028b786b55f
alexMA <almiddeleer@gmail.com>
2019-08-29 21:43:04 -0400

    Test for actual filter

--- e2e/cypress/integration/cards-page.js
+++ e2e/cypress/integration/cards-page.js
@@ -85,6 +85,24 @@ describe('Cards Page', function() {
       })
       .then(cards => {
         expect(allCardsLength).to.equal(cards.length);
+
+        // basic test - strength filter
+        cy.get(`${strengthPicker} input`)
+          .eq(1)
+          .click();
+        cy.get(cardSearchSubmit).click();
+        return cy.get(cardListCard);
+      })
+      .then(cards => {
+        expect(allCardsLength).to.be.above(cards.length);
+        cy.get(`${strengthPicker} input`)
+          .eq(1)
+          .click();
+        cy.get(cardSearchSubmit).click();
+        return cy.get(cardListCard);
+      })
+      .then(cards => {
+        expect(allCardsLength).to.equal(cards.length);
       });
   });
 });
