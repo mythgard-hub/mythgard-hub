88980faf23aab066d8d48db2323dd77111e4c7e9
alexMA <almiddeleer@gmail.com>
2019-08-30 22:22:19 -0400

    UI for tabs group. It currently has no behavior

--- next/components/tab-group.js
+++ next/components/tab-group.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { ThemeContext } from './theme-context';
 
 function TabGroup({ onChange, name, labels }) {
   const [selectedLabel, setLabel] = useState(labels[0]);
@@ -7,56 +8,65 @@ function TabGroup({ onChange, name, labels }) {
   useEffect(() => onChange(selectedLabel));
 
   return (
-    <div className="mg-tab-group" data-cy={`tabs_${name}`}>
-      <style jsx>{`
-        .mg-tab-group {
-          display: flex;
-          flex-wrap: nowrap;
-          font-size: 11px;
-        }
-        .tab {
-          padding: 5px;
-          border: 1px solid #458a9e;
-          background-color: #11222a;
-          border-bottom-left-radius: 0px;
-          border-bottom-right-radius: 0px;
-          color: #458a9e;
-          min-width: 100px;
-          text-align: center;
-          height: 30px;
-        }
-        .tab-spacer {
-          border-bottom: 1px solid #458a9e;
-          min-width: 15px;
-        }
-        .tab.selected {
-          border-bottom: none;
-        }
-        .tab-spacer:last-of-type {
-          border-bottom: 1px solid #458a9e;
-          min-width: 10px;
-          flex-grow: 1;
-        }
-      `}</style>
-      <div className="tab-spacer"></div>
-      {labels.map((label, i) => {
-        return (
-          <>
-            <button
-              key={2 * i}
-              className={`tab reset-button ${
-                label == selectedLabel ? 'selected' : ''
-              }`}
-              onClick={() => setLabel(label)}
-            >
-              {label}
-            </button>
-            <div key={2 * i + 1} className="tab-spacer"></div>
-          </>
-        );
-      })}
-      <div className="tab-spacer"></div>
-    </div>
+    <ThemeContext.Consumer>
+      {theme => (
+        <div className="mg-tab-group" data-cy={`tabs_${name}`}>
+          <style jsx>{`
+            .mg-tab-group {
+              display: flex;
+              flex-wrap: nowrap;
+              font-size: 11px;
+            }
+
+            .tab {
+              border: ${theme.tabBorder};
+              border-bottom-left-radius: 0px;
+              border-bottom-right-radius: 0px;
+              color: ${theme.tabColor};
+              min-width: 100px;
+              padding: 0 5px;
+              text-align: center;
+              height: 30px;
+            }
+            .tab.selected {
+              border-bottom: none;
+            }
+            .tab:focus {
+              outline: ${theme.border};
+              outline-offset: 6px;
+            }
+            .tab:hover {
+              background-color: ${theme.backgroundLight};
+            }
+
+            .tab-spacer {
+              border-bottom: ${theme.tabBorder};
+              min-width: 15px;
+            }
+            .tab-spacer:last-of-type {
+              flex-grow: 1;
+            }
+          `}</style>
+          <div className="tab-spacer"></div>
+          {labels.map((label, i) => {
+            const selectedClass = label == selectedLabel ? 'selected' : '';
+            return (
+              <>
+                <button
+                  key={2 * i}
+                  className={`tab reset-button ${selectedClass}`}
+                  onClick={() => setLabel(label)}
+                >
+                  {label}
+                </button>
+                <div key={2 * i + 1} className="tab-spacer"></div>
+              </>
+            );
+          })}
+          <div className="tab-spacer"></div>
+        </div>
+      )}
+    </ThemeContext.Consumer>
   );
 }
 
--- next/components/theme-context.js
+++ next/components/theme-context.js
@@ -4,10 +4,13 @@ import { mgColors, mgFonts } from '../lib/theme.js';
 export const themes = {
   dark: {
     background: mgColors.blackPearl,
+    backgroundLight: mgColors.blackPearlLight,
     fontColor: mgColors.white,
     fontColorSelected: mgColors.blue,
     sectionBackground: mgColors.mediumGray,
     sectionBorder: `2px solid ${mgColors.lightBlue}`,
+    tabBorder: `1px solid ${mgColors.lightBlue}`,
+    tabColor: mgColors.lightBlue,
     border: `1px solid ${mgColors.orange}`,
     fontFamily: mgFonts.exo2
   }
