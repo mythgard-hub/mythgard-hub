192a5720fd7f6198bb745b1270de313fdfbf95bc
Lily Pytel <lily.pytel@gmail.com>
2019-08-31 20:46:47 -0400

    add author to deck component + cleanup
    - add author line
    - remove query component
    - remove query to another file

--- next/components/deck.js
+++ next/components/deck.js
@@ -1,28 +1,10 @@
 import PropTypes from 'prop-types';
-import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
+import { useQuery } from 'react-apollo-hooks';
 import ErrorMessage from './error-message';
 import DeckCardList from './deck-card-list';
 import DeckExport from './deck-export';
 import { initializeDeckBuilder } from '../lib/deck-utils';
-
-export const deckCardsQuery = gql`
-  query($id: Int!) {
-    deck(id: $id) {
-      id
-      name
-      cardDecks {
-        nodes {
-          quantity
-          card {
-            name
-            id
-          }
-        }
-      }
-    }
-  }
-`;
+import { deckCardsQuery } from '../lib/deck-queries';
 
 const getDeckToExport = (deckCards, deckName, path = null, power = null) => {
   const deckToExport = initializeDeckBuilder();
@@ -37,30 +19,30 @@ const getDeckToExport = (deckCards, deckName, path = null, power = null) => {
 };
 
 export default function Deck({ deck }) {
+  const { loading, error, data } = useQuery(deckCardsQuery, {
+    variables: { id: deck.id }
+  });
+
+  if (error) return <ErrorMessage message="Error loading decks." />;
+  if (loading) return <div>Loading</div>;
+
+  const cards = data.deck ? data.deck.cardDecks.nodes : [];
+  const { power, path, author } = deck;
+  const deckToExport = getDeckToExport(cards, deck.name, path, power);
+  const authorName = (author && author.username) || 'unknown';
+
   return (
-    <Query query={deckCardsQuery} variables={{ id: deck.id }}>
-      {({ loading, error, data }) => {
-        if (error) return <ErrorMessage message="Error loading decks." />;
-        if (loading) return <div>Loading</div>;
-
-        const cards = data.deck ? data.deck.cardDecks.nodes : [];
-        const { power, path } = deck;
-        const deckToExport = getDeckToExport(cards, deck.name, path, power);
-
-        return (
-          <>
-            <h1 className="deckName">{deck.name}</h1>
-            <h2>Power</h2>
-            {power ? power.name : 'No Power Selected'}
-            <h2>Path</h2>
-            {path ? path.name : 'No Path Selected'}
-            <h2>Cards</h2>
-            <DeckCardList deckCards={cards} />
-            <DeckExport deckInProgress={deckToExport} />
-          </>
-        );
-      }}
-    </Query>
+    <>
+      <h1 className="deckName">{deck.name}</h1>
+      <div>by {authorName}</div>
+      <h2>Power</h2>
+      {power ? power.name : 'No Power Selected'}
+      <h2>Path</h2>
+      {path ? path.name : 'No Path Selected'}
+      <h2>Cards</h2>
+      <DeckCardList deckCards={cards} />
+      <DeckExport deckInProgress={deckToExport} />
+    </>
   );
 }
 Deck.propTypes = {
