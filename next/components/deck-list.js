6d4b751f434ad15e08d3705f44eb25c9ffdc1468
Lily Pytel <lily.pytel@gmail.com>
2019-08-31 20:51:55 -0400

    Add author information to decklist

--- next/components/deck-list.js
+++ next/components/deck-list.js
@@ -4,18 +4,23 @@ import PropTypes from 'prop-types';
 export default function DeckList({ decks }) {
   return (
     <ul>
-      {decks.map((deck, index) => (
-        <li data-cy="deckListItem" key={deck.id}>
-          <Link href={`/deck?id=${deck.id}`} key={index}>
-            <a>
-              {index + 1}. name: {deck.name}
-            </a>
-          </Link>
-        </li>
-      ))}
+      {decks.map((deck, index) => {
+        const author = deck && deck.author ? deck.author.username : 'unknown';
+
+        return (
+          <li data-cy="deckListItem" key={deck.id}>
+            <Link href={`/deck?id=${deck.id}`} key={index}>
+              <a>
+                {index + 1}. name: {deck.name}. by: {author}
+              </a>
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 }
+
 DeckList.propTypes = {
   decks: PropTypes.array
 };
