e8aebafb3f4b9cde3d7f9f5af73318b6f013827d
Alex Middeleer <almiddeleer@gmail.com>
2019-09-01 10:59:19 -0400

    Cards paths powers tabs (part 1 - all form, no function) (#59)
    * Start work on a tabs group component
    
    * Tab group styles looking deece
    
    * UI for tabs group. It currently has no behavior
    
    * abstract and decouple

--- next/components/theme-context.js
+++ next/components/theme-context.js
@@ -4,12 +4,17 @@ import { mgColors, mgFonts } from '../lib/theme.js';
 export const themes = {
   dark: {
     background: mgColors.blackPearl,
+    backgroundLight: mgColors.blackPearlLight,
     fontColor: mgColors.white,
     fontColorSelected: mgColors.blue,
     sectionBackground: mgColors.mediumGray,
     sectionBorder: `2px solid ${mgColors.lightBlue}`,
+    tabBorder: `1px solid ${mgColors.lightBlue}`,
+    tabColor: mgColors.lightBlue,
     border: `1px solid ${mgColors.orange}`,
-    fontFamily: mgFonts.exo2
+    fontFamily: mgFonts.exo2,
+    buttonOutline: `1px solid ${mgColors.orange}`,
+    buttonOutlineOffset: '6px'
   }
 };
 
