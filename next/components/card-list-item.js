955c24c41d00da572a14c539b50abddcb7dceb33
alexMA <almiddeleer@gmail.com>
2019-09-04 22:05:35 -0400

    hover image

--- next/components/card-list-item.js
+++ next/components/card-list-item.js
@@ -9,30 +9,49 @@ const getPath = (name, set = 'core') => {
   return `${cdn}/${set.toLowerCase()}/s/${nameToImage(name)}.png`;
 };
 
+const getHoverPath = (name, set = 'core') => {
+  return `${cdn}/${set.toLowerCase()}/m/${nameToImage(name)}.png`;
+};
+
 export default function CardListItem({ card, onClick }) {
   const imgAlt = card.name;
   const imgPath = getPath(card.name, card.set || undefined);
+  const imgPathMedium = getHoverPath(card.name, card.set || undefined);
 
   return (
     <>
       <style jsx>{`
-        img {
+        .cardListImg {
           width: 160px;
+        }
+        .imgWrapper {
+          display: inline-block;
+          position: relative;
+        }
+        // hover image
+        .imgWrapper:hover::before {
+          content: url(${imgPathMedium});
+          width: 320px;
+          position: absolute;
+          top: -64px;
+          left: -80px; // relative to small img
+          z-index: 2;
         }
       `}</style>
       {onClick && (
-        <span
+        <div
           role="button"
           data-cy="cardListCard"
+          className="imgWrapper"
           onClick={e => onClick(e, card)}
         >
-          <img src={imgPath} alt={imgAlt} />
-        </span>
+          <img className="cardListImg" src={imgPath} alt={imgAlt} />
+        </div>
       )}
       {!onClick && (
         <Link href={`/card?id=${card.id}`}>
-          <a data-cy="cardListCard">
-            <img src={imgPath} alt={imgAlt} />
+          <a data-cy="cardListCard" className="imgWrapper">
+            <img className="cardListImg" src={imgPath} alt={imgAlt} />
           </a>
         </Link>
       )}
