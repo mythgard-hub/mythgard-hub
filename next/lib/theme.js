b9c99a0ede888301e307fac1dd0b0b502f86431b
Lily Pytel <lily.pytel@gmail.com>
2019-09-04 22:14:49 -0400

    Add some colors, theme and global styles

--- next/lib/theme.js
+++ next/lib/theme.js
@@ -1,11 +1,12 @@
 export const mgColors = {
   blackPearl: '#11222A',
-  blackPearlLight: '#1a3541',
+  blackPearlLight: '#1A3541',
   orange: '#F1810B',
   blue: '#4EB8E7',
   white: '#FFF',
   lightGray: '#D1D1D1',
   mediumGray: '#333333',
+  grayBlue: '#1C2D35',
   lightBlue: '#458A9E'
 };
 
