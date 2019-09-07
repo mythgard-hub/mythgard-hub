9dc3febdb813ab051bae36f76fbaa4aa243e3cf1
Lily Pytel <lily.pytel@gmail.com>
2019-09-07 12:17:19 -0400

    Convert to functional component

--- next/components/deck-search-form.js
+++ next/components/deck-search-form.js
@@ -30,6 +30,17 @@ export default function DeckSearchForm(props) {
     });
   };
 
+  const handleClear = e => {
+    e && e.preventDefault();
+    setName('');
+    setCardIds([]);
+    setFactionNames([]);
+    setIsOnlyFactions(true);
+    setUpdatedTime('150');
+    setAuthorName('');
+    onSubmit({});
+  };
+
   const { error, data } = useQuery(allCardsQuery);
 
   let cardSearchElement = null;
@@ -117,7 +128,9 @@ export default function DeckSearchForm(props) {
           value="Apply"
           onClick={handleSubmit}
         />
-        <button>Clear</button>
+        <button onClick={handleClear} data-cy="deckSearchClear">
+          Clear
+        </button>
       </div>
     </form>
   );
