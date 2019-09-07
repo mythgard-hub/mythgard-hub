85dc9cd89875eebbf711a3fbe86d9f8b12be9977
alexMA <almiddeleer@gmail.com>
2019-09-07 13:09:14 -0400

    author name, created time

--- next/components/new-decks.js
+++ next/components/new-decks.js
@@ -4,6 +4,7 @@ import gql from 'graphql-tag';
 import ErrorMessage from './error-message.js';
 import { useContext } from 'react';
 import { ThemeContext } from '../components/theme-context.js';
+import Link from 'next/link';
 
 const decksQuery = gql`
   query decks {
@@ -11,6 +12,11 @@ const decksQuery = gql`
       nodes {
         id
         name
+        created
+        author {
+          id
+          username
+        }
       }
     }
   }
@@ -31,15 +37,35 @@ function NewDecks() {
         }
 
         .deckPreview {
-          border: ${theme.border};
+          border: ${theme.sectionBorder};
           display: block;
+          margin-bottom: 10px;
+          padding: 10px;
+        }
+
+        .deckName :global(a) {
+          color: ${theme.deckNameColor};
+          font-weight: bold;
+          text-decoration: none;
+          font-size: 1.1em;
         }
       `}</style>
       <ul className="deckList">
         {decks.map((deck, i) => {
           return (
             <li key={i} className="deckPreview">
-              {deck.name}
+              {/* TODO - duplicate code in deck-list.js*/}
+              <div className="deckName">
+                <Link href={`/deck?id=${deck.id}`}>{deck.name}</Link>
+                <div className="deckAuthor">
+                  by{' '}
+                  {(deck.author &&
+                    deck.author.username &&
+                    deck.author.username) ||
+                    'unknown'}{' '}
+                  {deck.created}
+                </div>
+              </div>
             </li>
           );
         })}
