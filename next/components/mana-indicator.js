1b39ae4831bd97082c6d734949fa79ead64f5292
alexMA <almiddeleer@gmail.com>
2019-09-07 17:33:44 -0400

    fix wrapping of 3rd column

--- next/components/mana-indicator.js
+++ next/components/mana-indicator.js
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { ThemeContext } from './theme-context';
 
+// TODO - this is actually an essence indicator
 export default function ManaIndicator({ mana }) {
   const theme = useContext(ThemeContext);
   const iconUrl = `${process.env.MG_CDN}/filters/essence.png`;
--- next/components/new-decks.js
+++ next/components/new-decks.js
@@ -20,6 +20,7 @@ function NewDecks() {
       <style jsx>{`
         .deckList {
           list-style: none;
+          padding: 0;
         }
       `}</style>
       <ul className="deckList">
