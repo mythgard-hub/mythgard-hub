3c1bd24f2a6f0333f6a8304548dadac05074e358
alexMA <almiddeleer@gmail.com>
2019-09-08 18:20:40 -0400

    Finish the function

--- scripts/search-decks-for-cards.sql
+++ scripts/search-decks-for-cards.sql
@@ -1,10 +1,10 @@
-SELECT deck.name,
-    card_deck.id as cardDeckId,
-    card.name,
-    card_faction.id as cardFactionId,
-    faction.name
-   FROM deck
+create function search_decks_for_cards(card1 integer, card2 Integer, card3 Integer, card4 Integer, card5 Integer)
+
+  returns setof deck as $$
+    select deck.*
+    FROM deck
      LEFT JOIN card_deck ON (card_deck.deck_id = deck.id)
      LEFT JOIN card ON (card.id = card_deck.card_id)
-     LEFT JOIN card_faction ON (card.id = card_faction.card_id)
-     LEFT JOIN faction ON (faction.id = card_faction.faction_id)
+   WHERE card.id IN (card1, card2, card3, card4, card5)
+   GROUP BY deck.id;
+  $$ language sql stable;
