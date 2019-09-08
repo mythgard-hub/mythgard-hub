09e6e5c0c4e62ce8447713e3ac5694d71e89dc1a
Lily Pytel <lily.pytel@gmail.com>
2019-09-08 17:33:31 -0400

    Create a component for filtering by circled numbers

---
+++ next/components/numeric-filter-group.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function NumericFilterGroup({ selected, onChange }) {
+  const cdn = process.env.MG_CDN;
+  const images = [
+    {
+      key: 1,
+      label: '1',
+      link: `${cdn}/filters/Filter-Icons_0000s_0001s_0005_1.png`
+    },
+    {
+      key: 2,
+      label: '2',
+      link: `${cdn}/filters/Filter-Icons_0000s_0001s_0004_2.png`
+    },
+    {
+      key: 3,
+      label: '3',
+      link: `${cdn}/filters/Filter-Icons_0000s_0001s_0003_3.png`
+    },
+    {
+      key: 4,
+      label: '4',
+      link: `${cdn}/filters/Filter-Icons_0000s_0001s_0002_4.png`
+    },
+    {
+      key: 5,
+      label: '5',
+      link: `${cdn}/filters/Filter-Icons_0000s_0001s_0001_5.png`
+    },
+    {
+      key: 6,
+      label: '6+',
+      link: `${cdn}/filters/Filter-Icons_0000s_0001s_0000_6%2B.png`
+    }
+  ];
+
+  const onClick = image => {
+    if (selected.indexOf(image.label) > -1) {
+      onChange(selected.filter(s => s !== image.label));
+      return;
+    }
+
+    onChange([...selected, image.label]);
+  };
+
+  return (
+    <div>
+      <style jsx>{`
+        img {
+          cursor: pointer;
+          height: 30px;
+          margin-right: 20px;
+          opacity: 0.6;
+        }
+
+        img:hover {
+          opacity: 0.8;
+        }
+
+        img.selected {
+          opacity: 1;
+          box-shadow: 0px 0px 5px #fff;
+          border-radius: 50%;
+        }
+      `}</style>
+      {images.map(i => (
+        <img
+          key={i.key}
+          src={i.link}
+          className={selected.indexOf(i.label) > -1 ? 'selected' : null}
+          onClick={() => onClick(i)}
+        />
+      ))}
+    </div>
+  );
+}
+
+NumericFilterGroup.propTypes = {
+  selected: PropTypes.array,
+  onChange: PropTypes.func
+};
+
+NumericFilterGroup.defaultProps = {
+  selected: []
+};
