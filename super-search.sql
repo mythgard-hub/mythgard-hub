0b0d535d27e9a5c16a64490fce0c26c4ef58ef1e
alexMA <almiddeleer@gmail.com>
2019-09-09 20:53:57 -0400

    add date modified to super deck search

--- super-search.sql
+++ super-search.sql
@@ -13,6 +13,7 @@ CREATE INDEX author_name_index ON account
 -- PARAMS:
 -- deckName str or null - name of deck (prefix only, so finds dragons given drag but not given rag)
 -- authorName str or null - name of author (prefix only, so finds alex given al but not given lex)
+-- modified str or null - modified on or after given date (e.g. "2019-07-11")
 -- numCards int         - number of cardN params that are not null (see next 5 params)
 --                        needed for searching decks that have specific cards in them. TODO - refactor this out
 -- card1    int or null - id of card being searched for (search supports at most 5 cards)
@@ -20,27 +21,30 @@ CREATE INDEX author_name_index ON account
 -- card3    int or null - id of card being searched for (search supports at most 5 cards)
 -- card4    int or null - id of card being searched for (search supports at most 5 cards)
 -- card5    int or null - id of card being searched for (search supports at most 5 cards)
-create function search_decks(deckName varchar(255), authorName varchar(255), numCards integer, card1 integer, card2 Integer, card3 Integer, card4 Integer, card5 Integer)
+create function search_decks(deckName varchar(255), authorName varchar(255), deckModified date, numCards integer, card1 integer, card2 Integer, card3 Integer, card4 Integer, card5 Integer)
   returns setof deck as $$
-    select deck.*
-    FROM deck
-     LEFT JOIN card_deck ON (card_deck.deck_id = deck.id)
-     LEFT JOIN card ON (card.id = card_deck.card_id)
-     LEFT JOIN account ON (account.id = deck.author_id)
-   -- deck name filter
-   WHERE (deckName is NULL or to_tsvector('simple', deck.name) @@ to_tsquery('simple',deckName || ':*'))
-   -- author name filter
-     AND (authorName is NULL or to_tsvector('simple', account.username) @@ to_tsquery('simple',authorName || ':*'))
-         -- cards filter
-     AND (numCards is null or numCards = 0 or (card.id in (card1, card2, card3, card4, card5)))
 
-   GROUP BY deck.id
-   -- Ensures that card filter requires *all* the specified cards, rather than *any* of them.
-   HAVING numCards is null or numCards = 0 or count(distinct card.id) = numCards
-   LIMIT 500;
+    select deck.* FROM deck
+      LEFT JOIN card_deck ON (card_deck.deck_id = deck.id)
+      LEFT JOIN card ON (card.id = card_deck.card_id)
+      LEFT JOIN account ON (account.id = deck.author_id)
+
+    -- deck name filter
+    WHERE (deckName is NULL or to_tsvector('simple', deck.name) @@ to_tsquery('simple',deckName || ':*'))
+    -- author name filter
+    AND (authorName is NULL or to_tsvector('simple', account.username) @@ to_tsquery('simple',authorName || ':*'))
+    -- modification date filter
+    AND (deckModified is NULL or deck.modified >= deckModified)
+    -- cards filter
+    AND (numCards is null or numCards = 0 or (card.id in (card1, card2, card3, card4, card5)))
+
+    GROUP BY deck.id
+    -- Ensures that card filter requires *all* the specified cards, rather than *any* of them.
+    HAVING numCards is null or numCards = 0 or count(distinct card.id) = numCards
+    LIMIT 500;
   $$ language sql stable;
 
-select * from search_decks(null, null, null, null, null, null, null, null);
+select * from search_decks(null, null, null, null, null, null, null, null, null);
 
 -------------------------
 
