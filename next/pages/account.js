741c2ff122784132493219106043e1c684fe4e1c
Evan Sheffield <evan.c.sheffield@gmail.com>
2019-09-16 13:29:20 -0400

    refactor: Make account page a functional component

--- next/pages/account.js
+++ next/pages/account.js
@@ -1,31 +1,18 @@
-import React from 'react';
+import { useContext, useState } from 'react';
 import Layout from '../components/layout';
 import UserContext from '../components/user-context';
 import { ApolloConsumer } from 'react-apollo';
 import updateUsername from '../lib/mutations/update-username';
 import Link from 'next/link';
 
-class Account extends React.Component {
-  constructor(props, context) {
-    super(props, context);
+function Account() {
+  const { user, updateUser } = useContext(UserContext);
+  const [username, setUsername] = useState(user.username);
 
-    this.state = {
-      username: context.user.username
-    };
-  }
+  const areSettingsValid = () => !!username;
 
-  static get requiresAuth() {
-    return true;
-  }
-
-  areSettingsValid = () => {
-    return !!this.state.username;
-  };
-
-  handleSubmit = apolloClient => {
-    if (!this.areSettingsValid()) return;
-    const { user, updateUser } = this.context;
-    const { username } = this.state;
+  const handleSubmit = apolloClient => {
+    if (!areSettingsValid()) return;
     updateUsername(apolloClient, user.id, username)
       .then(({ data }) => {
         updateUser(data.updateAccount.account);
@@ -35,58 +22,54 @@ class Account extends React.Component {
       });
   };
 
-  render() {
-    const { user } = this.context;
-    const { username } = this.state;
-    return (
-      <Layout
-        title="Mythgard Hub | Account Settings"
-        desc="Account settings for Mythgard Hub"
-      >
-        <h1>My Profile</h1>
+  return (
+    <Layout
+      title="Mythgard Hub | Account Settings"
+      desc="Account settings for Mythgard Hub"
+    >
+      <h1>My Profile</h1>
 
-        <h2>Settings</h2>
+      <h2>Settings</h2>
 
-        <div className="stack">
-          <div>
-            <Link href="/auth/logout">Log out</Link>
-          </div>
-          <div>
-            <label>Email: </label>
-            <span>{user.email}</span>
-          </div>
-          <div>
-            <label>User Name: </label>
-            <input
-              data-cy="username"
-              type="text"
-              name="username"
-              onChange={e => {
-                this.setState({ username: e.target.value });
-              }}
-              value={username}
-            />
-          </div>
-          <ApolloConsumer>
-            {client => (
-              <button
-                type="submit"
-                value="Save"
-                style={{ width: '25%' }}
-                onClick={() => {
-                  this.handleSubmit(client);
-                }}
-              >
-                Save
-              </button>
-            )}
-          </ApolloConsumer>
+      <div className="stack">
+        <div>
+          <Link href="/auth/logout">Log out</Link>
         </div>
-      </Layout>
-    );
-  }
+        <div>
+          <label>Email: </label>
+          <span>{user.email}</span>
+        </div>
+        <div>
+          <label>User Name: </label>
+          <input
+            data-cy="username"
+            type="text"
+            name="username"
+            onChange={e => {
+              setUsername(e.target.value);
+            }}
+            value={username}
+          />
+        </div>
+        <ApolloConsumer>
+          {client => (
+            <button
+              type="submit"
+              value="Save"
+              style={{ width: '25%' }}
+              onClick={() => {
+                handleSubmit(client);
+              }}
+            >
+              Save
+            </button>
+          )}
+        </ApolloConsumer>
+      </div>
+    </Layout>
+  );
 }
 
-Account.contextType = UserContext;
+Account.requiresAuth = true;
 
 export default Account;
