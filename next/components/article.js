db1b209988870e278cb764d20705fcad7d08365c
jtrussell <jus.russell@gmail.com>
2019-09-18 20:41:29 -0400

    Use time lib, mark date as required

--- next/components/article.js
+++ next/components/article.js
@@ -1,30 +1,11 @@
 import PropTypes from 'prop-types';
 import { useContext } from 'react';
 import { ThemeContext } from './theme-context';
+import { dateToDeltaString } from '../lib/time';
 
 function Article({ title, url, description, date }) {
   const theme = useContext(ThemeContext);
-
-  const now = new Date();
-  const timeDelta = now.getTime() - date.getTime();
-  const hrsDelta = Math.ceil(timeDelta / (1000 * 60 * 60 * 24));
-  const daysDelta = Math.ceil(timeDelta / (1000 * 60 * 60 * 24));
-  const monthsDelta = Math.ceil(timeDelta / (1000 * 60 * 60 * 24 * 30));
-
-  let timeMsg;
-
-  if (monthsDelta > 12) {
-    timeMsg = 'Over a year ago';
-  } else if (monthsDelta > 1) {
-    timeMsg = `${monthsDelta} months ago`;
-  } else if (daysDelta > 1) {
-    timeMsg = `${daysDelta} days ago`;
-  } else if (hrsDelta > 1) {
-    timeMsg = `${hrsDelta} hours ago`;
-  } else {
-    timeMsg = 'Just now';
-  }
-
+  const timeMsg = dateToDeltaString(date);
   return (
     <>
       <style jsx>{`
@@ -64,7 +45,7 @@ Article.propTypes = {
   title: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  date: PropTypes.instanceOf(Date)
+  date: PropTypes.instanceOf(Date).isRequired
 };
 
 export default Article;
