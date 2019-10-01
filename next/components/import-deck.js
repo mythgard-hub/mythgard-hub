1d8b9c780358099827a1a6a82a87a5210de90a29
Alex Middeleer <almiddeleer@gmail.com>
2019-09-30 21:02:11 -0400

    Sometimes cards appeared in 2 columns on deckbuilder page (#153)

--- next/components/import-deck.js
+++ next/components/import-deck.js
@@ -77,6 +77,7 @@ export default function ImportDeck({
           margin-top: 10px;
           margin-bottom: 10px;
           max-width: 100%;
+          width: 100%;
         }
       `}</style>
       <textarea
--- scripts/rebuild-db-and-restart-graphql.sh
+++ scripts/rebuild-db-and-restart-graphql.sh
@@ -3,5 +3,5 @@
 docker-compose stop db
 docker-compose rm -vf db
 docker-compose build --no-cache db
-docker-compose stop express
+docker-compose stop -t 0 express
 docker-compose up -d
