8e984ba5b304f5380b53b4bc09343c6371aa8e96
Evan Sheffield <evan.c.sheffield@gmail.com>
2019-10-01 23:27:27 -0400

    Better display for artifact durability on card detail page

--- next/constants/supertypes.js
+++ next/constants/supertypes.js
@@ -1,5 +1,12 @@
 const cdn = `${process.env.MG_CDN}/filters/`;
 
+export const SUPERTYPES = {
+  minion: 'MINION',
+  spell: 'SPELL',
+  enchantment: 'ENCHANTMENT',
+  artifact: 'ARTIFACT'
+};
+
 export const SUPERTYPE_IMAGES = {
   minion: `${cdn}Filter-Icons_0000s_0004s_0000_minion.png`,
   spell: `${cdn}Filter-Icons_0000s_0004s_0001_spell.png`,
