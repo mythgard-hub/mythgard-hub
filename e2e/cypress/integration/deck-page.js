718503aaa970ee9489dfcf566899eee9aefed8b6
lily-pytel <lily.pytel@gmail.com>
2019-10-05 23:37:26 -0400

    Essence factions to deck (#160)
    * Add deck meta info to the single deck query
    
    * Safeguard just in cases
    
    * Consistent safeguard
    
    * Add essence and factions to deck page
    
    * Add ... to loading messages, it's the least we can do until we have an animation
    
    * Make a global style for that hr
    
    * Add a tiny assertion that there's essence and factions on the page
    
    * Add date created because why not?
    
    * Remove duplicate width

--- e2e/cypress/integration/deck-page.js
+++ e2e/cypress/integration/deck-page.js
@@ -4,6 +4,9 @@ describe('Deck Page', function() {
   });
   it('happy path card click', function() {
     cy.get('[data-cy="deckCardTable"]').should('have.length', 1);
+    cy.get('[data-cy="deckEssenceCell"]').should('be.visible');
+    cy.get('[data-cy="factionsIndicator"]').should('be.visible');
+    cy.get('[data-cy="deckCreatedDate"]').should('be.visible');
     cy.get('[data-cy="deckCardTable"] a:first').click();
     cy.location().should(location => {
       expect(location.pathname).to.contain('/card');
