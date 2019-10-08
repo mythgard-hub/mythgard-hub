eb8ede46cdd44b79ea0d60c1c60b9a3897380671
PapaCapricorn <39966530+PapaCapricorn@users.noreply.github.com>
2019-10-08 15:14:37 -0400

    Events page scaffolding

--- next/pages/events.js
+++ next/pages/events.js
@@ -14,7 +14,7 @@ function TournamentsPage() {
           padding-bottom: 10px;
         }
 
-        .announcement content {
+        .announcement announcementText {
           display: table-cell;
           vertical-align: middle;
           text-align: left;
@@ -31,9 +31,32 @@ function TournamentsPage() {
           color: ${mgColors.orange};
           text-decoration: none;
         }
+
         a:hover {
           color: ${mgColors.blue};
         }
+
+        .eventPanel {
+          display: table;
+          float: left;
+          width: 266px;
+          height: 90px;
+          padding: 10px;
+          border-style: solid;
+          border-color: ${mgColors.blue};
+          border-width: 1px;
+          margin-right: 5px;
+          background-color: ${mgColors.darkBlue};
+        }
+        .panelContent.eventName {
+          font-weight: 700;
+        }
+
+        .panelContent.eventDate {
+          font-size: 1em;
+          font-weight: 400;
+        }
+
       `}</style>
       <PageBanner image={PageBanner.IMG_EVENTS}>Events</PageBanner>
       <div className="announcement">
@@ -42,8 +65,8 @@ function TournamentsPage() {
           src="https://cdn.mythgardhub.com/identity/MGH_CrownLogo.png"
         />
 
-        <content>
-          <br />
+        // Reformatting to div based on input from Lily
+        <div className="announcementText">
           <h2>Thank you for your interest in Mythgard Events!</h2>
           <p>
             As <strong>Community</strong> and <strong>Official</strong>{' '}
@@ -57,8 +80,45 @@ function TournamentsPage() {
             </a>{' '}
             with the details.
           </p>
+        </div>
+      </div>
+
+      <h1>Upcoming Events</h1>
+
+      // WHY DO I EVEN NEED THIS? - can't get formatting to work without
+      <div style="clear: both;">&nbsp;</a></div>
+
+      <div class="eventPanel">
+        <content>
+          <div class="panelContent eventName">
+          <a href="https://teamrankstar.com/98-3-presents-moonlight-masquerade/" target="_blank">Moonlight Masquerade <img style="width: 12px;" src="https://cdn.mythgardhub.com/icons/newWindow.svg"></a></div>
+          <div class="panelOrganizer">98.3 Media</div>
+          <div class="panelContent eventDate">October 26th -27th, 2019</div>
         </content>
       </div>
+
+      <div class="eventPanel">
+        <content>
+          <div class="panelContent eventName">
+          <a href={`mailto:${process.env.EMAIL_MG_EVENTS}`}>List Your Event Here</a></div>
+          <div class="panelOrganizer">YOU</div>
+          <div class="panelContent eventDate">Date of your choice</div>
+        </content>
+      </div>
+
+      <div class="eventPanel">
+        <content>
+          <div class="panelContent eventName">
+          <a href={`mailto:${process.env.EMAIL_MG_EVENTS}`}>List Your Event Here</a></div>
+          <div class="panelOrganizer">YOU</div>
+          <div class="panelContent eventDate">Date of your choice</div>
+        </content>
+      </div>
+
+      <h1>Completed Events</h1>
+
+      Event results table to go here, for reference see <a href="https://mythgard-hub.github.io/events_template.html">mockup</a>
+
     </Layout>
   );
 }
