2b153fc7ac8b3bb7a33372564c44592176d809fe
PapaCapricorn <39966530+PapaCapricorn@users.noreply.github.com>
2019-10-10 15:59:15 -0400

    Wecolme banner editable decks

--- next/components/welcome-banner.js
+++ next/components/welcome-banner.js
@@ -77,7 +77,7 @@ export default function WelcomeBanner() {
         Contact
       </a>
       <span className="welcome-message">
-        Welcome to the Mythgard Hub Beta Launch
+        Welcome to the Mythgard Hub Beta Launch - NEW: Decks are now editable for registered accounts
       </span>
 
       <span className="spacer"></span>
