66661f71e2f94c7b6948dc44622e1a4fc703ae16
Lily Pytel <lily.pytel@gmail.com>
2019-10-12 14:04:34 -0400

    Scroll to top after query is completed, minus the initial load

--- next/components/all-decks.js
+++ next/components/all-decks.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import { useQuery } from 'react-apollo-hooks';
+import React, { useRef } from 'react';
+import { useQuery } from '@apollo/react-hooks';
 import { useState } from 'react';
 import ErrorMessage from './error-message';
 import DeckList from './deck-list';
 import { allDecksQuery } from '../lib/deck-queries';
 import PagingControls from './paging-controls.js';
+import { scrollToTopOfList } from '../lib/navigation-utils';
 
 const pageSize = 50;
 
 export default function AllDecks() {
+  const listRef = React.createRef();
+  const dataLoaded = useRef(false);
   const [currentPage, setPage] = useState(0);
   const { loading, error, data } = useQuery(allDecksQuery, {
     variables: {
       first: pageSize,
       offset: currentPage * pageSize
+    },
+    onCompleted: _ => {
+      if (!dataLoaded.current) {
+        setTimeout(() => {
+          dataLoaded.current = true;
+        }, 1000);
+      } else {
+        scrollToTopOfList(listRef);
+      }
     }
   });
 
@@ -23,7 +35,6 @@ export default function AllDecks() {
   if (data && !data.decks) return <div>No decks found</div>;
 
   const totalCount = data.decks.totalCount;
-  const listRef = React.createRef();
 
   return (
     <div ref={listRef}>
@@ -38,7 +49,6 @@ export default function AllDecks() {
         }
       `}</style>
       <PagingControls
-        listRef={listRef}
         currentPage={currentPage}
         setPage={setPage}
         pageSize={pageSize}
