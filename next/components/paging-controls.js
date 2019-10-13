30fa95f579b4a07c8e962fc507e91e7a57e9d7f5
Lily Pytel <lily.pytel@gmail.com>
2019-10-12 23:32:56 -0400

    Fix import

--- next/components/paging-controls.js
+++ next/components/paging-controls.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { hasNextPage, hasPrevPage, rangeMin, rangeMax } from '../lib/paging.js';
-import { scrollToTopOfElement } from '../lib/ui-utils.js/index.js';
+import { scrollToTopOfElement } from '../lib/ui-utils';
 
 function PagingControls({
   currentPage,
