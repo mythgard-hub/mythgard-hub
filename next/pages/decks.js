264491ceb9da83ed2c4b2d773523ddd2cfd024af
alexMA <almiddeleer@gmail.com>
2019-10-17 21:49:20 -0400

    Partial: deck search uses url but form ui doesn't update yet

--- next/pages/decks.js
+++ next/pages/decks.js
@@ -1,26 +1,37 @@
 import React, { useState } from 'react';
-import AllDecks from '../components/all-decks';
 import DeckSearchForm from '../components/deck-search-form';
 import SomeDecks from '../components/some-decks';
 import PageBanner from '../components/page-banner';
 import Layout from '../components/layout';
+import { useRouter } from 'next/router';
 
-const hasSearch = function(searchQuery) {
-  return (
-    searchQuery &&
-    (searchQuery.name ||
-      searchQuery.cardIds ||
-      searchQuery.factionNames ||
-      searchQuery.updatedTime ||
-      searchQuery.authorName)
-  );
+const searchQueryDefaults = {
+  name: '',
+  cardIds: [],
+  factionNames: [],
+  isOnlyFactions: true,
+  updatedTime: '150',
+  authorName: ''
 };
 
 export default function DecksPage() {
-  const [searchQuery, setSearchQuery] = useState({});
+  const router = useRouter();
+  const initialSearchQuery = { ...searchQueryDefaults };
+  for (const entry of Object.entries(router.query)) {
+    const [name, value] = entry;
+    if (value) {
+      initialSearchQuery[name] =
+        typeof searchQueryDefaults[name] !== 'object'
+          ? value
+          : value.split(',');
+    }
+  }
+  const [searchQuery, setSearchQuery] = useState(initialSearchQuery);
 
   const handleSearchSubmit = searchQuery => {
     setSearchQuery(searchQuery);
+    const pageParams = new URLSearchParams(searchQuery).toString();
+    router.replace(`/decks?${pageParams}`);
   };
 
   return (
@@ -31,13 +42,13 @@ export default function DecksPage() {
         }
       `}</style>
       <PageBanner image={PageBanner.IMG_DECKS}>Decks</PageBanner>
-      <DeckSearchForm onSubmit={handleSearchSubmit} />
+      <DeckSearchForm
+        onSubmit={handleSearchSubmit}
+        searchQuery={searchQuery}
+        defaultQuery={{ ...searchQueryDefaults }}
+      />
       <h1>Results</h1>
-      {hasSearch(searchQuery) ? (
-        <SomeDecks search={searchQuery} />
-      ) : (
-        <AllDecks />
-      )}
+      <SomeDecks search={searchQuery} />
     </Layout>
   );
 }
