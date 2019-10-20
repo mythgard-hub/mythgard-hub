7b34811d64d16c0702e1452ebb4b3ada9340845b
Alex Middeleer <almiddeleer@gmail.com>
2019-10-20 18:05:13 -0400

    Cards page - Preserve filters when returning to search feature from a different page (#181)
    * It works but clear filter is broken
    
    * Clear is working but filter selection gui doesn't reset
    
    * Fix ui after clear filters
    
    * Refactor

--- next/pages/cards.js
+++ next/pages/cards.js
@@ -3,14 +3,43 @@ import Layout from '../components/layout';
 import PageBanner from '../components/page-banner';
 import { useState } from 'react';
 import CardSearchForm from '../components/card-search-form';
+import { useRouter } from 'next/router';
 
 export const twoColMinWidth = 925;
 
+const searchQueryDefaults = {
+  text: '',
+  factions: [],
+  supertypes: [],
+  manaCosts: [],
+  strengths: [],
+  defenses: [],
+  rarities: []
+};
+
 function CardsPage() {
-  const [searchQuery, setSearchQuery] = useState({});
+  const router = useRouter();
+  const initialSearchQuery = { ...searchQueryDefaults };
+  for (const entry of Object.entries(router.query)) {
+    const [name, value] = entry;
+    if (value) {
+      initialSearchQuery[name] =
+        typeof searchQueryDefaults[name] !== 'object'
+          ? value
+          : value.split(',');
+    }
+  }
+  const [searchQuery, setSearchQuery] = useState(initialSearchQuery);
 
   const handleSearchSubmit = searchQuery => {
     setSearchQuery(searchQuery);
+    const pageParams = new URLSearchParams(searchQuery).toString();
+    router.replace(`/cards?${pageParams}`);
+  };
+
+  const clearFilters = () => {
+    setSearchQuery({ ...searchQueryDefaults });
+    router.replace(`/cards`);
   };
 
   return (
@@ -19,7 +48,12 @@ function CardsPage() {
       desc="Browse and search for Mythgard cards"
     >
       <PageBanner image={PageBanner.IMG_CARDS}>Cards</PageBanner>
-      <CardSearchForm onSubmit={handleSearchSubmit.bind(this)}>
+      <CardSearchForm
+        onSubmit={handleSearchSubmit}
+        searchQuery={searchQuery}
+        defaultQuery={{ ...searchQueryDefaults }}
+        onClearFilters={clearFilters}
+      >
         <style jsx>{`
           :global(.cardList) {
             padding-left: 0;
