69afd870c93a7b62aef2c39c1faf055974f760a0
Lily Pytel <lily.pytel@gmail.com>
2019-10-19 20:32:12 -0400

    Maintain the hover delays

--- next/components/card-list-item.js
+++ next/components/card-list-item.js
@@ -24,14 +24,23 @@ export default function CardListItem({ card, onClick, options }) {
           display: inline-block;
           position: relative;
         }
-        // hover image
-        .imgWrapper:hover::before {
+        // bigger version of the image (hidden until hover)
+        .imgWrapper::before {
           content: url(${imgPathMedium});
           width: ${IMAGE_SIZES.hoverImageWidth}px;
           position: absolute;
           top: -${IMAGE_SIZES.hoverImageVerticalOffset}px;
           left: -${(IMAGE_SIZES.hoverImageWidth - smallImageWidth) / 2}px;
           z-index: 2;
+          opacity: 0;
+        }
+
+        @media (hover: hover) {
+          // Show the hover image (but only on devices that have hover)
+          .imgWrapper:hover::before {
+            opacity: 1;
+            transition-delay: 0.7s;
+          }
         }
       `}</style>
       {onClick && (
