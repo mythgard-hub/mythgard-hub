3cda8986c8c8892bfae932a54fed8cf829d4417f
Eduardo Sollet <edswor@hotmail.com>
2019-10-27 18:24:51 +0100

    Dynamic web previews cards decks

--- next/pages/card.js
+++ next/pages/card.js
@@ -1,23 +1,73 @@
 import { withRouter } from 'next/router';
+import Head from 'next/head';
 import ErrorMessage from '../components/error-message';
 import Card from '../components/card';
 import Layout from '../components/layout';
 import PageBanner from '../components/page-banner';
 import cardQuery from '../lib/queries/card-detail-query';
 import { useQuery } from 'react-apollo-hooks';
+import {
+  imagePathMedium as getImagePath,
+  mainFaction,
+  formatManaCost
+} from '../lib/card.js';
+
+const firstLetterUppercase = str => {
+  return !str || str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+};
 
 export default withRouter(({ router }) => {
   const { loading, error, data } = useQuery(cardQuery, {
     variables: { id: parseInt(router.query.id, 10) }
   });
-  if (loading) return null;
+  if (loading) return <div>Loading...</div>;
   if (error) return <ErrorMessage message={error.message} />;
+  if (!data || !data.card)
+    return <ErrorMessage message={'Card does not exist!'} />;
 
   return (
     <Layout
       title={`Mythgard Hub | Cards | ${data.card.name}`}
       desc={`Details and rulings for Mythgard card ${data.card.name}`}
     >
+      <Head>
+        <meta
+          key="og:title"
+          property="og:title"
+          content={`${data.card.name} | Mythgard Hub`}
+        />
+        <meta
+          key="og:site_name"
+          property="og:site_name"
+          content="Mythgard Hub"
+        />
+        <meta
+          key="og:description"
+          property="og:description"
+          content={`${firstLetterUppercase(
+            mainFaction(data.card)
+          )}, ${formatManaCost(data.card)}${data.card.gem} ${data.card.rarity &&
+            firstLetterUppercase(data.card.rarity)} ${data.card.supertype &&
+            firstLetterUppercase(data.card.supertype.toString())}, ${data.card
+            .subtype && firstLetterUppercase(data.card.subtype)}, Core`}
+        />
+        <meta
+          key="og:url"
+          property="og:url"
+          content={`https://mythgardhub.com${router.asPath}`}
+        />
+        <meta key="og:type" property="og:type" content="article" />
+        <meta
+          key="og:image"
+          property="og:image"
+          content={getImagePath(data.card.name, data.card.set)}
+        />
+        <meta
+          key="og:image:type"
+          property="og:image:type"
+          content="image/png"
+        />
+      </Head>
       <PageBanner image={PageBanner.IMG_CARDS}>Cards</PageBanner>
       <Card card={data.card} />
     </Layout>
--- next/pages/deck.js
+++ next/pages/deck.js
@@ -1,4 +1,5 @@
 import { withRouter } from 'next/router';
+import Head from 'next/head';
 import { useQuery } from 'react-apollo-hooks';
 import ErrorMessage from '../components/error-message';
 import Deck from '../components/deck';
@@ -9,18 +10,79 @@ export default withRouter(({ router }) => {
   const { error, loading, data } = useQuery(singleDeckQuery, {
     variables: { id: parseInt(router.query.id, 10) }
   });
+  if (loading) return <div>Loading...</div>;
+  if (error) return <ErrorMessage message={error.message} />;
+  if (!data || !data.deck)
+    return <ErrorMessage message={'Deck does not exist!'} />;
 
-  let pageElement = null;
+  const authorName =
+    (data.deck.author && data.deck.author.username) || 'unknown';
+  const metaData =
+    data.deck.deckPreviews &&
+    data.deck.deckPreviews.nodes &&
+    data.deck.deckPreviews.nodes[0];
+  const essenceCost = metaData && metaData.essenceCost;
+  const dateCreated =
+    metaData &&
+    metaData.deckCreated &&
+    new Date(metaData.deckCreated).toLocaleDateString('en-UK', {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric'
+    });
 
-  if (error) {
-    pageElement = <ErrorMessage message={error.message} />;
-  } else if (loading) {
-    pageElement = <div>Loading...</div>;
-  } else if (data && data.deck) {
-    pageElement = <Deck deck={data.deck} />;
-  } else {
-    pageElement = <ErrorMessage message={'Deck does not exist!'} />;
-  }
+  const factions = metaData && metaData.factions;
+  const colors = factions
+    .map(faction =>
+      faction
+        .replace('norden', 'B')
+        .replace('aztlan', 'Y')
+        .replace('oberos', 'R')
+        .replace('dreni', 'G')
+        .replace('parsa', 'O')
+        .replace('harmony', 'P')
+    )
+    .join('');
 
-  return <Layout>{pageElement}</Layout>;
+  return (
+    <Layout
+      title={`Mythgard Hub | Decks | ${data.deck.name}`}
+      desc={`Details for Mythgard deck ${data.deck.name}`}
+    >
+      <Head>
+        <meta
+          key="og:title"
+          property="og:title"
+          content={`${data.deck.name} by ${authorName} | Mythgard Hub`}
+        />
+        <meta
+          key="og:site_name"
+          property="og:site_name"
+          content="Mythgard Hub"
+        />
+        <meta
+          key="og:description"
+          property="og:description"
+          content={`Standard Mythgard Deck, ${colors}, ${essenceCost} Essence, Updated ${dateCreated}`}
+        />
+        <meta
+          key="og:url"
+          property="og:url"
+          content={`https://mythgardhub.com${router.asPath}`}
+        />
+        <meta key="og:type" property="og:type" content="article" />
+        <meta
+          key="og:image"
+          property="og:image"
+          content="https://cdn.mythgardhub.com/identity/og-image.jpg"
+        />
+        <meta
+          key="og:image:type"
+          property="og:image:type"
+          content="image/png"
+        />
+      </Head>
+      <Deck deck={data.deck} />
+    </Layout>
+  );
 });
