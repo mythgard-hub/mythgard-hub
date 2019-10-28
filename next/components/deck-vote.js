9968662a5edb41c711bd666dc2a435a6d8bc2861
alexMA <almiddeleer@gmail.com>
2019-10-27 20:01:01 -0400

    Deck vote removal is working

--- next/components/deck-vote.js
+++ next/components/deck-vote.js
@@ -2,15 +2,41 @@ import PropTypes from 'prop-types';
 import { useCallback, useContext, useState } from 'react';
 import { ApolloContext } from 'react-apollo';
 import upvoteDeck from '../lib/mutations/deck-upvote.js';
-// import removeDeckUpvote from '../lib/mutations/deck-remove-upvote.js';
+import removeDeckUpvote from '../lib/mutations/deck-remove-upvote.js';
 import UserContext from '../components/user-context';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+
+const deckVotesQuery = gql`
+  query deckAccountVotes($deckId: Int!, $accountId: Int!) {
+    deckVotes(
+      filter: {
+        deckId: { equalTo: $deckId }
+        accountId: { equalTo: $accountId }
+      }
+    ) {
+      nodes {
+        id
+      }
+    }
+  }
+`;
 
 let messageTimeoutHandle;
 
 function DeckVote({ deck }) {
   const { client } = useContext(ApolloContext);
   const { user } = useContext(UserContext);
+  const { data } = useQuery(deckVotesQuery, {
+    variables: {
+      deckId: deck.id,
+      accountId: user && user.id
+    }
+  });
+  const userDeckVote =
+    data && data.deckVotes && data.deckVotes.nodes && data.deckVotes.nodes[0];
   const [message, setMessage] = useState(null);
+
   const handleClick = useCallback(async () => {
     clearTimeout(messageTimeoutHandle);
     let resp;
@@ -21,7 +47,23 @@ function DeckVote({ deck }) {
         console.error(err);
       }
     }
-    setMessage(resp ? 'Vote Successful' : 'Unable to vote');
+    setMessage(resp ? 'Vote Successful' : 'Error voting');
+    messageTimeoutHandle = setTimeout(() => {
+      setMessage(null);
+    }, 2000);
+  });
+
+  const handleRemoveVote = useCallback(async () => {
+    clearTimeout(messageTimeoutHandle);
+    let resp;
+    try {
+      resp = await removeDeckUpvote(client, userDeckVote.id);
+    } catch (err) {
+      if (process.env.NODE_ENV === 'development') {
+        console.error(err);
+      }
+    }
+    setMessage(resp ? 'Unvote Successful' : 'Error removing vote');
     messageTimeoutHandle = setTimeout(() => {
       setMessage(null);
     }, 2000);
@@ -44,7 +86,8 @@ function DeckVote({ deck }) {
         }
       `}</style>
       <span className="voteCount">{votes}</span>
-      {canVote && <button onClick={handleClick}>Vote</button>}
+      {canVote && !userDeckVote && <button onClick={handleClick}>Vote</button>}
+      {userDeckVote && <button onClick={handleRemoveVote}>Remove Vote</button>}
       {message && <span>{message}</span>}
     </div>
   );
