5de4a6edb3a3815d2f3b6c37757944c43080ab3c
alexMA <almiddeleer@gmail.com>
2019-10-28 21:43:48 -0400

    Update migration

--- postgres/migrations/migration-1.3.3.sql
+++ postgres/migrations/migration-1.3.3.sql
@@ -20,3 +20,19 @@ CREATE OR REPLACE VIEW mythgard.deck_preview as
     On deck_vote.deck_id = deck.id
  GROUP BY deck.id
 ;
+
+ALTER TABLE mythgard.deck_vote ENABLE ROW LEVEL SECURITY;
+
+-- Admin users can make any changes and read all rows
+CREATE POLICY deck_vote_admin_all ON mythgard.deck_vote TO admin USING (true) WITH CHECK (true);
+-- Non-admins can read all rows
+CREATE POLICY deck_vote_all_view ON mythgard.deck_vote FOR SELECT USING (true);
+-- Rows can only be updated by their author
+CREATE POLICY deck_vote_insert_if_author
+  ON mythgard.deck_vote
+  FOR INSERT
+  WITH CHECK ("account_id" = mythgard.current_user_id());
+CREATE POLICY deck_vote_delete_if_author
+  ON mythgard.deck_vote
+  FOR DELETE
+  USING ("account_id" = mythgard.current_user_id());
