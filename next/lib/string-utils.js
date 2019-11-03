e54cd9250496b0707a1d9c22a7058353952338d3
Lily Pytel <lily.pytel@gmail.com>
2019-11-03 17:25:15 -0500

    Move function to utils

--- next/lib/string-utils.js
+++ next/lib/string-utils.js
@@ -5,3 +5,25 @@ export const firstLetterUppercase = str => {
     return '';
   }
 };
+
+export const ordinalized = i => {
+  try {
+    const j = i % 10,
+      k = i % 100;
+
+    if (j == 1 && k != 11) {
+      return i + 'st';
+    }
+
+    if (j == 2 && k != 12) {
+      return i + 'nd';
+    }
+
+    if (j == 3 && k != 13) {
+      return i + 'rd';
+    }
+    return i + 'th';
+  } catch (e) {
+    return '';
+  }
+};
