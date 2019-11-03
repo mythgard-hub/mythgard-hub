c1deaeb4c3ad91d7da1f21d7507831a9cb8637ee
Lily Pytel <lily.pytel@gmail.com>
2019-11-03 18:02:48 -0500

    Always show the edit button

--- next/components/deck-card-table-edit-meta.js
+++ next/components/deck-card-table-edit-meta.js
@@ -1,47 +1,43 @@
-import { useContext } from 'react';
 import PropTypes from 'prop-types';
 
 export default function DeckCardsTableEditMeta(props) {
   const { metaName, metaValue, onEditClick } = props;
 
   return (
-    metaValue || (
-      <div className="no-meta-value-selected-container">
-        <style jsx>{`
-          .no-meta-value-selected-container {
-            padding: 5px;
-            display: flex;
-            justify-content: space-between;
-          }
-          .edit-button {
-            padding: 0;
-            border: none;
-            text-transform: lowercase;
-            font-style: normal;
-            font-weight: normal;
-          }
-          .edit-button:before {
-            text-decoration: none;
-            content: '\u25b6';
-            margin-right: 3px;
-            font-size: 10px;
-          }
-          .no-meta-value {
-            text-transform: uppercase;
-          }
-        `}</style>
-        <div className="no-meta-value">No {metaName} selected</div>
-        <div>
-          <button
-            data-cy="editMetaValue"
-            className="edit-button"
-            onClick={onEditClick}
-          >
-            edit
-          </button>
-        </div>
+    <div className="no-meta-value-selected-container">
+      <style jsx>{`
+        .no-meta-value-selected-container {
+          display: flex;
+          justify-content: space-between;
+        }
+        .edit-button {
+          padding: 0;
+          border: none;
+          text-transform: lowercase;
+          font-style: normal;
+          font-weight: normal;
+        }
+        .edit-button:before {
+          text-decoration: none;
+          content: '\u25b6';
+          margin-right: 3px;
+          font-size: 10px;
+        }
+        .no-meta-value {
+          text-transform: uppercase;
+        }
+      `}</style>
+      {metaValue || <div className="no-meta-value">No {metaName} selected</div>}
+      <div>
+        <button
+          data-cy="editMetaValue"
+          className="edit-button"
+          onClick={onEditClick}
+        >
+          edit
+        </button>
       </div>
-    )
+    </div>
   );
 }
 
