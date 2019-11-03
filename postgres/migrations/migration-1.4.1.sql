81ff4c458cb64a7e03edafe172a358afd27c2ace
lily-pytel <lily.pytel@gmail.com>
2019-11-03 18:36:26 -0500

    Fix essence cost multi faction (#210)
    * Add a card with multiple factions
    
    * Remove one of the factions
    
    * Separate essence calcualtions to a function
    
    * Fix test
    
    * Fix second part of test
    
    * Fix test
    
    * Fix actual test that was broken and add a test
    
    * Create migration-1.4.1.sql

---
+++ postgres/migrations/migration-1.4.1.sql
@@ -0,0 +1,43 @@
+CREATE OR REPLACE FUNCTION deck_essence_cost (IN deckId INTEGER) 
+RETURNS INTEGER AS $$
+  DECLARE
+    essence_cost INTEGER;
+  BEGIN
+    SELECT sum(essence_costs.essence * card_deck.quantity)::int into essence_cost
+    FROM mythgard.deck
+    JOIN mythgard.card_deck
+      ON card_deck.deck_id = deck.id
+    JOIN mythgard.card
+      ON card_deck.card_id = card.id
+    LEFT JOIN mythgard.essence_costs
+      On essence_costs.rarity = card.rarity
+    WHERE deck.id = $1
+    GROUP BY deck.id;
+    
+    RETURN essence_cost;
+  
+  END;
+  $$ language 'plpgsql';
+
+CREATE OR REPLACE VIEW mythgard.deck_preview as
+  SELECT deck.id as deck_id,
+         deck.name as deck_name,
+         deck.created as deck_created,
+         array_agg(DISTINCT faction.name) as factions,
+         deck_essence_cost(deck.id)::int as essence_cost,
+         count(DISTINCT deck_vote)::int as votes
+  FROM mythgard.deck
+  JOIN mythgard.card_deck
+    ON card_deck.deck_id = deck.id
+  JOIN mythgard.card
+    ON card_deck.card_id = card.id
+  LEFT JOIN mythgard.card_faction
+    ON (card.id = card_faction.card_id and card_faction.faction_id is not null)
+  LEFT JOIN mythgard.faction
+    On faction.id = card_faction.faction_id
+  LEFT JOIN mythgard.essence_costs
+    On essence_costs.rarity = card.rarity
+  LEFT JOIN mythgard.deck_vote
+    On deck_vote.deck_id = deck.id
+ GROUP BY deck.id
+;
