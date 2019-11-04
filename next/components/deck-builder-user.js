6922f2ee6020097878c068c6b9c3878ff57f8d02
Lily Pytel <lily.pytel@gmail.com>
2019-11-03 23:01:36 -0500

    Create a component to show the user building the deck

---
+++ next/components/deck-builder-user.js
@@ -0,0 +1,61 @@
+import { useContext } from 'react';
+import Link from 'next/link';
+import { ThemeContext } from './theme-context';
+import UserContext from '../components/user-context';
+
+export default function DeckBuilderUser() {
+  const theme = useContext(ThemeContext);
+  const { user } = useContext(UserContext);
+
+  const userElement = (
+    <Link href={user ? '/account' : '/auth/google'}>
+      <a data-cy="userLink" className="link">
+        {user?.username || 'Guest'}
+      </a>
+    </Link>
+  );
+
+  return (
+    <div className="deck-author">
+      <style jsx>{`
+        .deck-author {
+          display: flex;
+          justify-content: space-between;
+          margin: 10px 0;
+        }
+        .login-link,
+        .user-link {
+          text-decoration: none;
+          color: ${theme.fontColorAccent};
+        }
+        .login-link:before {
+          text-decoration: none;
+          content: '\u25b6';
+          margin-right: 3px;
+          font-size: 10px;
+        }
+        .login-link {
+          font-size: 14px;
+        }
+        .user-link {
+          font-size: inherit;
+        }
+      `}</style>
+      <div>
+        by{' '}
+        <Link href={user ? '/account' : '/auth/google'}>
+          <a data-cy="userLink" className="user-link">
+            {user?.username || 'Guest'}
+          </a>
+        </Link>
+      </div>
+      {!user && (
+        <Link href="/auth/google">
+          <a data-cy="loginToSave" className="login-link">
+            Login/Register
+          </a>
+        </Link>
+      )}
+    </div>
+  );
+}
