6feac193afd5bf237b5117c08dba4be8b1007896
Lily Pytel <lily.pytel@gmail.com>
2019-11-06 22:43:45 -0500

    Fix typo

--- next/lib/string-utils.js
+++ next/lib/string-utils.js
@@ -8,7 +8,7 @@ export const firstLetterUppercase = str => {
 
 export const ordinalized = i => {
   try {
-    if (!i && i !=== 0) return '';
+    if (!i && i !== 0) return '';
 
     const j = i % 10,
       k = i % 100;
