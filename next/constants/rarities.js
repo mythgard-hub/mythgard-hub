91dfd79871056c7fe188e5a8c84177235020363a
Eduardo Sollet <edswor@hotmail.com>
2019-11-08 23:13:11 +0100

    try/catch for better error handling Modified the rare color (more gold like)

--- next/constants/rarities.js
+++ next/constants/rarities.js
@@ -17,20 +17,22 @@ export const RARITY_MAX_CARDS = {
 export const RARITY_COLORS = {
   common: '#926950',
   uncommon: '#A5A5A5',
-  rare: '#FDF007',
+  rare: '#ccac00',
   mythic: '#33A0E0'
 };
 
 export const getRarityImage = rarity => {
-  if (!rarity) {
-    rarity = 'common';
+  try {
+    return RARITY_IMAGES[rarity.toLowerCase()];
+  } catch (e) {
+    return RARITY_IMAGES.common;
   }
-  return RARITY_IMAGES[rarity.toLowerCase()];
 };
 
 export const getRarityColor = rarity => {
-  if (!rarity) {
-    rarity = 'common';
+  try {
+    return RARITY_COLORS[rarity.toLowerCase()];
+  } catch (e) {
+    return RARITY_COLORS.common;
   }
-  return RARITY_COLORS[rarity.toLowerCase()];
 };
