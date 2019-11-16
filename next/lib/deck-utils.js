e57c1b1564aa18f82771ff107f603f2fc9d360be
Lily Pytel <lily.pytel@gmail.com>
2019-11-16 11:25:30 -0500

    Add type and archetype to deck in progress

--- next/lib/deck-utils.js
+++ next/lib/deck-utils.js
@@ -12,7 +12,9 @@ export const initializeDeckBuilder = () => {
     deckCoverArt: '',
     mainDeck: {},
     sideboard: [],
-    errors: []
+    errors: [],
+    archetype: 'Unknown',
+    type: 'Standard'
   };
 };
 
