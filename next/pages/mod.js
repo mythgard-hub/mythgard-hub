f83feed51d3a3a182fbe9e13a389bc76b96e20fc
alexMA <almiddeleer@gmail.com>
2019-11-18 20:56:10 -0500

    Add a layout

--- next/pages/mod.js
+++ next/pages/mod.js
@@ -1,3 +1,6 @@
-export default () => {
-  return 'hello mods page';
+import Layout from '../components/layout';
+const ModeratorPage = () => {
+  return <Layout>Hello mods page </Layout>;
 };
+
+export default ModeratorPage;
