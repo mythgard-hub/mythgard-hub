e3e22add1b44700dfcd1e72e486f58aa7e94205a
alexMA <almiddeleer@gmail.com>
2019-11-18 21:57:39 -0500

    Make moderator page redirect if not logged in

--- next/pages/mod.js
+++ next/pages/mod.js
@@ -1,6 +1,21 @@
+import { useContext, useEffect } from 'react';
 import Layout from '../components/layout';
+import UserContext from '../components/user-context';
+import Router from 'next/router';
+
 const ModeratorPage = () => {
-  return <Layout>Hello mods page </Layout>;
+  const { user } = useContext(UserContext);
+  useEffect(() => {
+    if (!user) {
+      Router.push('/error');
+    }
+  });
+
+  return (
+    <Layout>
+      <h1>Mythgard moderators</h1>
+    </Layout>
+  );
 };
 
 export default ModeratorPage;
