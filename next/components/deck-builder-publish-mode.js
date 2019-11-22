5e447c675f5c2eb02e330ba9c54fc0d4f68d2759
Lily Pytel <lily.pytel@gmail.com>
2019-11-21 21:44:22 -0500

    Save changes to deck after publishing

--- next/components/deck-builder-publish-mode.js
+++ next/components/deck-builder-publish-mode.js
@@ -1,9 +1,13 @@
+import { useContext } from 'react';
 import PropTypes from 'prop-types';
 import Router from 'next/router';
+import { ApolloConsumer } from 'react-apollo';
+import { saveDeckWithCards } from '../lib/deck-utils.js';
 import { PAGE_MODES } from '../constants/deck-builder';
 import { clearDeckInProgress } from '../lib/deck-utils';
 import { ARCHETYPES, TYPES } from '../constants/deck';
 import DeckBuilderPublishDropdown from './deck-builder-publish-dropdown';
+import UserContext from '../components/user-context';
 
 export default function DeckBuilderPublishMode({
   pageMode,
@@ -12,6 +16,8 @@ export default function DeckBuilderPublishMode({
   setDeckInProgress,
   deckInProgress
 }) {
+  const { user } = useContext(UserContext);
+
   if (pageMode !== PAGE_MODES.PUBLISH) return null;
 
   return (
@@ -24,15 +30,23 @@ export default function DeckBuilderPublishMode({
           margin-bottom: 15px;
         }
       `}</style>
-      <button
-        className="publish-button"
-        onClick={() => {
-          Router.push(`/deck?id=${deckId}`);
-          clearDeckInProgress(setDeckInProgress);
-        }}
-      >
-        Publish Decklist
-      </button>
+      <ApolloConsumer>
+        {client => (
+          <button
+            className="publish-button"
+            onClick={() => {
+              saveDeckWithCards(client, deckId, deckInProgress, user.id).then(
+                () => {
+                  Router.push(`/deck?id=${deckId}`);
+                  clearDeckInProgress(setDeckInProgress);
+                }
+              );
+            }}
+          >
+            Publish Decklist
+          </button>
+        )}
+      </ApolloConsumer>
       <button onClick={() => setPageMode(PAGE_MODES.TABLE)}>Cancel</button>
       <DeckBuilderPublishDropdown
         title="Deck Archetype"
