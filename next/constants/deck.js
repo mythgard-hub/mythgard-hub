2886a702e1b09389e9825ca9685e9d74818ea7bb
Lily Pytel <lily.pytel@gmail.com>
2019-11-22 22:56:21 -0500

    Since the db is different than the display, expand the constants

--- next/constants/deck.js
+++ next/constants/deck.js
@@ -18,14 +18,19 @@ export const DECK_SIZES = {
 
 export const DECK_BUILDER_TABS = ['Cards', 'Paths', 'Powers'];
 
-export const TYPES = ['Standard', 'Gauntlet', 'Tournament'];
+export const TYPES = [
+  { value: ['STANDARD'], label: 'Standard' },
+  { value: ['GAUNTLET'], label: 'Gauntlet' },
+  { value: ['TOURNAMENT'], label: 'Tournament' },
+  { value: ['TWOVTWO'], label: '2v2' }
+];
 
 export const ARCHETYPES = [
-  'Unknown',
-  'Aggro',
-  'Aggro Midrange',
-  'Midrange',
-  'Control Midrange',
-  'Control',
-  'Combo'
+  { value: ['UNKNOWN'], label: 'Unknown' },
+  { value: ['AGGRO'], label: 'Aggro' },
+  { value: ['AGGRO', 'MIDRANGE'], label: 'Aggro Midrange' },
+  { value: ['MIDRANGE'], label: 'Midrange' },
+  { value: ['CONTROL', 'MIDRANGE'], label: 'Control Midrange' },
+  { value: ['CONTROL'], label: 'Control' },
+  { value: ['COMBO'], label: 'Combo' }
 ];
