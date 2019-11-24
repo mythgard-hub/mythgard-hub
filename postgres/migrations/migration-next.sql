c2fcff3588e197a190d659924a6d110fc52e5767
Lily Pytel <lily.pytel@gmail.com>
2019-11-24 08:59:19 -0500

    More matching original branch

--- postgres/migrations/migration-next.sql
+++ postgres/migrations/migration-next.sql
@@ -28,22 +28,21 @@ CREATE OR REPLACE VIEW mythgard.deck_preview as
  GROUP BY deck.id
 ;
 
-CREATE TABLE mythgard.account_moderator(
+CREATE TABLE mythgard.account_moderator (
   id SERIAL PRIMARY KEY,
   account_id integer,
   FOREIGN KEY (account_id)
     REFERENCES mythgard.account (id)
-    ON DELETE CASCADE
-);
+    ON DELETE CASCADE);
 
 DROP POLICY deck_update_if_author on deck;
 CREATE POLICY deck_update_if_author_or_mod
   ON mythgard.deck
   FOR UPDATE
   USING (("author_id" = mythgard.current_user_id())
-    OR
-    (exists (select * from mythgard.account_moderator
-        where account_id = mythgard.current_user_id())));
+        OR
+        (exists (select * from mythgard.account_moderator
+                where account_id = mythgard.current_user_id())));
 
 
 --                            search_decks
