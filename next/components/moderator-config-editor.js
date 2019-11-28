3c2ebe174ba5e80ea4f9e59d64934d591ed60d73
alexMA <almiddeleer@gmail.com>
2019-11-28 13:45:40 -0500

    Continuing to build form

--- next/components/moderator-config-editor.js
+++ next/components/moderator-config-editor.js
@@ -1,5 +1,17 @@
 import useConfig from '../lib/use-config.js';
 import ModeratorEditArticle from '../components/moderator-edit-article.js';
+import { useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+const configMutation = gql`
+  mutation setConfig($newConfig: JSON!) {
+    updateSiteConfig(input: { id: 1, patch: { config: $newConfig } }) {
+      siteConfig {
+        config
+      }
+    }
+  }
+`;
 
 function ModeratorConfigEditor() {
   const { config, error, loading } = useConfig();
@@ -12,11 +24,23 @@ function ModeratorConfigEditor() {
     return 'loading config';
   }
 
-  console.log('config: ', config);
+  const [updateConfig] = useMutation(configMutation, {
+    update() {
+      window.location.reload();
+    },
+    onError() {
+      alert('That failed. Please check values and try again');
+    }
+  });
+
+  const setArticle = article => console.log('article: ', article);
 
   const topMediaInputs = config.topMedia.map((media, i) => (
     <div key={i}>
-      <ModeratorEditArticle article={media}></ModeratorEditArticle>
+      <ModeratorEditArticle
+        article={media}
+        setArticle={setArticle}
+      ></ModeratorEditArticle>
       <hr />
     </div>
   ));
--- next/components/moderator-edit-article.js
+++ next/components/moderator-edit-article.js
@@ -1,24 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { handleInputChangeHooks } from '../lib/form-utils.js';
 
-function ModeratorEditArticle({ article }) {
+function ModeratorEditArticle({ article, setArticle }) {
+  const [title, setTitle] = useState(article.title);
+  const onChangeTitle = handleInputChangeHooks(setTitle);
+  const [url, setUrl] = useState(article.url);
+  const onChangeUrl = handleInputChangeHooks(setUrl);
+  const [desc, setDesc] = useState(article.description);
+  const onChangeDesc = handleInputChangeHooks(setDesc);
+  const [date, setDate] = useState(article.date);
+  const onChangeDate = handleInputChangeHooks(setDate);
+  const [author, setAuthor] = useState(article.author);
+  const onChangeAuthor = handleInputChangeHooks(setAuthor);
+
+  const onClick = () =>
+    setArticle({
+      title,
+      url,
+      description: desc,
+      date,
+      author
+    });
   return (
     <div>
       <label>
-        Title: <input type="text" value={article.title} />
+        Title: <input type="text" value={title} onChange={onChangeTitle} />
       </label>
       <label>
-        Url: <input type="text" value={article.url} />
+        Url: <input type="text" value={url} onChange={onChangeUrl} />
       </label>
       <label>
-        Description: <input type="text" value={article.description} />
+        Description: <input type="text" value={desc} onChange={onChangeDesc} />
       </label>
       <label>
-        Date: <input type="text" value={article.date} />
+        Date: <input type="text" value={date} onChange={onChangeDate} />
       </label>
       <label>
-        Author: <input type="text" value={article.author} />
+        Author: <input type="text" value={author} onChange={onChangeAuthor} />
       </label>
+      <div>
+        <button onClick={onClick}>Save</button>
+      </div>
     </div>
   );
 }
@@ -26,7 +50,8 @@ function ModeratorEditArticle({ article }) {
 ModeratorEditArticle.defaultProps = {};
 
 ModeratorEditArticle.propTypes = {
-  article: PropTypes.object
+  article: PropTypes.object,
+  setArticle: PropTypes.func
 };
 
 export default ModeratorEditArticle;
