a856e27bfc0b0a7dbe5e018301eb899c5d53f7a6
alexMA <almiddeleer@gmail.com>
2019-12-08 15:18:26 -0500

    partial

--- postgres/wip.sql
+++ postgres/wip.sql
@@ -1,19 +1,30 @@
+-- def hot(ups, downs, date):
+--     s = score(ups, downs)
+--     order = log(max(abs(s), 1), 10)
+--     sign = 1 if s > 0 else -1 if s < 0 else 0
+--     seconds = epoch_seconds(date) - 1134028003
+--     return round(sign * order + seconds / 45000, 7)
+
+
 drop function if exists mythgard.epochSeconds;
 create function mythgard.epochSeconds(timestamp) returns int as $$
   select extract(epoch from $1)::int;
 $$ language sql;
 
-drop function if exists mythgard.deckCreatedEpochSeconds;
-create function mythgard.deckCreatedEpochSeconds(deckid integer) returns int as $$
-
-  select mythgard.epochSeconds(max(deck.created)) from mythgard.deck where deck.id = deckid;
-
-$$ language sql;
-
 drop function if exists mythgard.hotness;
+create function mythgard.hotness(ups integer, creation timestamp) returns int as $$
 
-create function mythgard.hotness(deckid integer) returns int as $$
-  select mythgard.deckCreatedEpochSeconds(deckid);
+  select mythgard.epochSeconds(creation);
+
 $$ language sql;
 
-select * FROM mythgard.hotness(1);
+drop function if exists mythgard.deckHotness;
+create function mythgard.deckHotness(deckId integer) returns int as $$
+
+  select mythgard.hotness(mythgard.deck_votes(deckId), created)
+  from mythgard.deck
+  where deck.id = deckId;
+
+$$ language sql;
+
+select * FROM mythgard.deckHotness(1);
