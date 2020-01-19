465f0e88a6dc1c76b1008503e01e13c15efa2f4c
Alex Middeleer <almiddeleer@gmail.com>
2020-01-19 14:04:44 -0500

    Update hotness (#258)
    * Updated hotness calc. Tests need to be updated
    
    * Stop treating decks with 2 and 3 votes as identically hot

---
+++ postgres/migrations/migration-1.7.0.sql
@@ -0,0 +1,14 @@
+drop function if exists mythgard.hotnessOrder;
+create function mythgard.hotnessOrder(votes integer) returns numeric as $$
+  select log(2, greatest(votes + 1, 1));
+$$ language sql stable;
+
+drop function if exists mythgard.hotness;
+create function mythgard.hotness(votes integer, creation timestamp) returns double precision as $$
+
+  select round(
+    ((mythgard.hotnessSign(votes) * mythgard.hotnessOrder(votes))
+      + (mythgard.hotnessSeconds(creation) / 5 / 45000))::numeric
+  , 7)::double precision;
+
+$$ language sql stable;
