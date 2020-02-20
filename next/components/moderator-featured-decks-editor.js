9c0a43c6e69a2f9e101b31da8e9d3d73657f4ca7
alexMA <almiddeleer@gmail.com>
2020-02-19 19:44:15 -0500

    replace span with input

--- next/components/moderator-featured-decks-editor.js
+++ next/components/moderator-featured-decks-editor.js
@@ -4,11 +4,23 @@ import PropTypes from 'prop-types';
 // <style jsx>{}</style>
 function moderatorFeaturedDecksEditor() {
   const deckIds = [1, 2, 3];
+  const onChangeDeckId = id => {
+    console.log('hi', id);
+  };
   return (
     <div>
       <h1>Edit Featured Decks</h1>
-      {deckIds.map(id => {
-        return <span key={id}>{id}</span>;
+      {deckIds.map((id, index) => {
+        return (
+          <label key={id} htmlFor={`deckId${index}`}>
+            deck Id:{id}
+            <input
+              name={`deckId${index}`}
+              value={id}
+              onChange={onChangeDeckId}
+            />
+          </label>
+        );
       })}
     </div>
   );
