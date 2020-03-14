65a15d67d4f94784b0a3b3ce65bcf2f8a31ebfae
alexMA <almiddeleer@gmail.com>
2020-03-14 18:04:50 -0400

    Load actual top decks, with delete button and add form

--- next/components/moderator-featured-decks-editor.js
+++ next/components/moderator-featured-decks-editor.js
@@ -1,27 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useQuery } from '@apollo/react-hooks';
+import { useState } from 'react';
+import CompactDeckList from './compact-deck-list.js';
+import { topDeckPreviewsQuery as decksQuery } from '../lib/deck-queries.js';
 
 // <style jsx>{}</style>
 function moderatorFeaturedDecksEditor() {
-  const deckIds = [1, 2, 3];
+  const { loading, error, data } = useQuery(decksQuery);
   const onChangeDeckId = id => {
     console.log('hi', id);
   };
+
+  if (error) {
+    return 'error loading top decks';
+  }
+
+  if (loading) {
+    return 'loading...';
+  }
+
+  const deckPreviews =
+    (data && data.deckPreviews && data.deckPreviews.nodes) || [];
+  const deckIds = deckPreviews.map(x => x.deck.id);
+
   return (
     <div>
       <h1>Edit Featured Decks</h1>
+
       {deckIds.map((id, index) => {
         return (
-          <label key={id} htmlFor={`deckId${index}`}>
-            deck Id:{id}
-            <input
-              name={`deckId${index}`}
-              value={id}
-              onChange={onChangeDeckId}
-            />
-          </label>
+          <div key={index}>
+            <label>
+              deck Id: {id} <button style={{ width: 100 }}>Delete</button>
+            </label>
+          </div>
         );
       })}
+      <label>
+        new deck Id: <input />
+        <button style={{ width: 100 }}>Save</button>
+      </label>
     </div>
   );
 }
