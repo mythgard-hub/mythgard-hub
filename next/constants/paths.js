40e10532bcc17121654f6b31a696750ee256326a
Lily Pytel <lily.pytel@gmail.com>
2020-04-25 20:06:40 -0400

    put paranoia in place

--- next/constants/paths.js
+++ next/constants/paths.js
@@ -1,20 +1,25 @@
 const cdn = `${process.env.MG_CDN}/filters/`;
 
-export const matchPathNameToIcon = pathName => {
-  const lowerCaseName = pathName.toLowerCase();
+export const matchPathNameToIcon = path => {
+  try {
+    const pathName = path.name;
+    const lowerCaseName = pathName.toLowerCase();
 
-  switch (lowerCaseName) {
-    case 'disk of circadia':
-      return `${cdn}DiskOfCircadia.png`;
-    case 'fires of creation':
-      return `${cdn}FiresOfCreation.png`;
-    case 'journey of souls':
-      return `${cdn}JourneyOfSouls.png`;
-    case "rainbow's end":
-      return `${cdn}RainbowsEnd.png`;
-    case 'turn of seasons':
-      return `${cdn}TurnOfSeasons.png`;
-    default:
-      return null;
+    switch (lowerCaseName) {
+      case 'disk of circadia':
+        return `${cdn}DiskOfCircadia.png`;
+      case 'fires of creation':
+        return `${cdn}FiresOfCreation.png`;
+      case 'journey of souls':
+        return `${cdn}JourneyOfSouls.png`;
+      case "rainbow's end":
+        return `${cdn}RainbowsEnd.png`;
+      case 'turn of seasons':
+        return `${cdn}TurnOfSeasons.png`;
+      default:
+        return null;
+    }
+  } catch (e) {
+    return null;
   }
 };
--- next/constants/powers.js
+++ next/constants/powers.js
@@ -1,24 +1,29 @@
 const cdn = `${process.env.MG_CDN}/filters/`;
 
-export const matchPowerNameToIcon = powerName => {
-  const lowerCaseName = powerName.toLowerCase();
+export const matchPowerNameToIcon = power => {
+  try {
+    const powerName = power.name;
+    const lowerCaseName = powerName.toLowerCase();
 
-  switch (lowerCaseName) {
-    case 'foresight':
-      return `${cdn}foresight_slug.png`;
-    case 'protect':
-      return `${cdn}protect_slug.png`;
-    case 'impel':
-      return `${cdn}impel_slug.png`;
-    case 'infuse':
-      return `${cdn}infuse_slug.png`;
-    case 'reconstruct':
-      return `${cdn}reanimate_slug.png`;
-    case 'mend':
-      return `${cdn}mend.png`;
-    case 'smite':
-      return `${cdn}smite_slug.png`;
-    default:
-      return null;
+    switch (lowerCaseName) {
+      case 'foresight':
+        return `${cdn}foresight_slug.png`;
+      case 'protect':
+        return `${cdn}protect_slug.png`;
+      case 'impel':
+        return `${cdn}impel_slug.png`;
+      case 'infuse':
+        return `${cdn}infuse_slug.png`;
+      case 'reconstruct':
+        return `${cdn}reanimate_slug.png`;
+      case 'mend':
+        return `${cdn}rejuvenate_slug.png`;
+      case 'smite':
+        return `${cdn}smite_slug.png`;
+      default:
+        return null;
+    }
+  } catch (e) {
+    return null;
   }
 };
