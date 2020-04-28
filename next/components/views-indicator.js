7b955c1eb56dae8c1572a48089edf7f329d4651f
Lily Pytel <lily.pytel@gmail.com>
2020-04-28 19:33:34 -0400

    add component for deck views

---
+++ next/components/views-indicator.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+
+export default function ViewsIndicator({ views }) {
+  return (
+    <span className="viewsIndicator">
+      <style jsx>{`
+        img {
+          max-height: 12px;
+          margin-right: 3px;
+        }
+        span {
+          padding-left: 5px;
+        }
+      `}</style>
+      <img src={`${process.env.MG_CDN}/icons/eyeball.svg`} />
+      <span className="viewsCell" data-cy="viewsCell">
+        {views}
+      </span>
+    </span>
+  );
+}
+
+ViewsIndicator.propTypes = {
+  views: PropTypes.number
+};
