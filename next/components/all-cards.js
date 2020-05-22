b81db82175fb1bc68a96f561a6dee96fc484eb6d
Alex Middeleer <almiddeleer@gmail.com>
2020-05-22 15:49:19 -0400

    Order cards factions correctly (#275)
    * It might work
    
    * more test
    
    * Add sort by mana
    
    * add sort by gems
    
    * oops, gems didn't matter - sort by mana then name. Checked actual game client to confirm

--- next/components/all-cards.js
+++ next/components/all-cards.js
@@ -2,6 +2,7 @@ import { useQuery } from '@apollo/react-hooks';
 import ErrorMessage from './error-message';
 import CardList from './card-list';
 import PropTypes from 'prop-types';
+import { cardSort } from '../lib/card.js';
 
 import allCardsQuery from '../lib/queries/all-cards-query';
 
@@ -12,6 +13,8 @@ export default function AllCards(props) {
   if (error) return <ErrorMessage message={error.message} />;
   if (loading) return <div>Loading cards...</div>;
   if (!data || !data.cards) return null;
+
+  data.cards.nodes = data.cards.nodes.sort(cardSort);
 
   const cards = data.cards;
   return <CardList onCardClick={onCardClick} cards={cards.nodes} />;
