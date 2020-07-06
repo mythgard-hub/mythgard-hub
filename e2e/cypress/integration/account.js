67d667c47ff4429de0e3c810a14cedc513997152
Lily Pytel <lily.pytel@gmail.com>
2020-07-05 22:48:55 -0400

    update tests

--- e2e/cypress/integration/account.js
+++ e2e/cypress/integration/account.js
@@ -3,5 +3,6 @@ describe('Account', function() {
     cy.visit('/account?name=lsv');
     cy.get('[data-cy="profile-name"]').should('contain', 'lsv');
     cy.get('[data-cy="user-deck"]').should('have.length', 2);
+    cy.get('[data-cy="userAvatar"]').should('have.length', 1);
   });
 });
