50ca866c35054bbcb3afcb8f20f72fae357c4fb2
alexMA <almiddeleer@gmail.com>
2020-07-19 13:07:36 -0400

    replace user-decks with top decks and newest decks on pub prof page

--- next/components/public-account.js
+++ next/components/public-account.js
@@ -2,7 +2,8 @@ import Layout from '../components/layout';
 import { useQuery } from '@apollo/react-hooks';
 import PropTypes from 'prop-types';
 import UserProfile from './profile.js';
-import UserDecks from '../components/user-decks';
+import UserNewestDecks from './user-newest-decks.js';
+import UserTopDecks from './user-top-decks.js';
 import { useContext } from 'react';
 import { ThemeContext } from './theme-context';
 import gql from 'graphql-tag';
@@ -36,7 +37,6 @@ export default function PublicAccount({ username }) {
   let user, viewMoreAuthorDecksLink;
   try {
     user = data && data.accountByUsername;
-    viewMoreAuthorDecksLink = `/decks?updatedTime=100000&authorName=${user.username}&sortBy=hot`;
   } catch (e) {
     return <Layout>No profile found for user: {username}</Layout>;
   }
@@ -51,14 +51,53 @@ export default function PublicAccount({ username }) {
           background-color: #1c2d35;
           padding-bottom: 40px;
         }
+        .homePageColumns {
+          display: flex;
+          flex-wrap: wrap;
+          margin: 0;
+          padding: 10px 0;
+        }
+
+        .mg-column + .mg-column {
+          margin-left: ${theme.spacing * 2}px;
+        }
+
+        .mg-column {
+          flex: 1;
+        }
+
+        .homePageColumns h2 {
+          text-align: center;
+          font-size: 1.2em;
+          font-weight: 700;
+          font-style: italic;
+          text-transform: uppercase;
+        }
+        @media only screen and (max-width: 575.98px) {
+          .mg-column {
+            width: 100%;
+            flex: none;
+          }
+
+          .mg-column + .mg-column {
+            margin-left: 0;
+          }
       `}</style>
       <Layout>
         <div className="public-profile">
           <UserProfile user={user} />
           <h2>{user.username}&apos;s Decks</h2>
-          <UserDecks userId={user.id} limit={3} />
-          <div>
-            <a href={viewMoreAuthorDecksLink}>View More</a>
+          <div className="homePageColumns">
+            <div className="mg-column">
+              <h2>Top Decks</h2>
+              <hr className="orangeGrade" />
+              <UserTopDecks userId={user.id} username={user.username} />
+            </div>
+            <div className="mg-column">
+              <h2>Newest Decks</h2>
+              <hr className="orangeGrade" />
+              <UserNewestDecks userId={user.id} username={user.username} />
+            </div>
           </div>
         </div>
       </Layout>
---
+++ next/components/user-newest-decks.js
@@ -0,0 +1,73 @@
+import PropTypes from 'prop-types';
+import { useQuery } from '@apollo/react-hooks';
+import ErrorMessage from './error-message';
+import CompactDeckList from './compact-deck-list.js';
+import { userNewestDecksQuery } from '../lib/deck-queries';
+
+function UserDecks({ userId, username, limit }) {
+  const { loading, error, data } = useQuery(userNewestDecksQuery, {
+    variables: { userId, limit }
+  });
+
+  if (error) return <ErrorMessage message={error.message} />;
+  if (loading) return <div>Loading...</div>;
+
+  const userDeckCount =
+    data && data.deckPreviews && data.deckPreviews.totalCount;
+
+  const viewMoreAuthorDecksLink = `/decks?updatedTime=100000&authorName=${username}&sortBy=hot`;
+
+  return (
+    <>
+      <style jsx>{`
+        .deck-list {
+          padding: 0 20px;
+          list-style: none;
+        }
+        .view-more {
+          font-size: 0.7em;
+          margin-top: -5px;
+          margin-right: 20px;
+          font-weight: 600;
+          text-align: right;
+          color: #ffffff;
+          text-decoration: none;
+          text-transform: uppercase;
+        }
+        .view-more-button {
+          cursor: pointer;
+        }
+        .view-more-button:before {
+          text-decoration: none;
+          content: '\u25b6';
+          font-size: 80%;
+          margin-right: 5px;
+        }
+      `}</style>
+      <CompactDeckList
+        loading={loading}
+        error={error}
+        data={data}
+        hideDate={true}
+        cyData="newestDecks"
+      />
+      {limit !== -1 && userDeckCount > limit && (
+        <div className="view-more">
+          <a href={viewMoreAuthorDecksLink}>View More</a>
+        </div>
+      )}
+    </>
+  );
+}
+
+UserDecks.propTypes = {
+  userId: PropTypes.number.isRequired,
+  limit: PropTypes.number,
+  username: PropTypes.string
+};
+
+UserDecks.defaultProps = {
+  limit: 3
+};
+
+export default UserDecks;
