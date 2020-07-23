93b4fdaad845101035f0e430f1ce49787e3133c4
lily-pytel <lily.pytel@gmail.com>
2020-07-23 07:45:27 -0400

    Add icon to deck page (#288)
    * add author info to deck query
    
    * Add user avatar to deck page
    
    * update test and test data

--- e2e/cypress/integration/deck-page.js
+++ e2e/cypress/integration/deck-page.js
@@ -1,22 +1,26 @@
 describe('Deck Page', function() {
   beforeEach(() => {
-    cy.visit('/deck/3');
+    cy.visit('/deck/11');
   });
   it('deck page elements', function() {
     // Meta tags
-    cy.get('head title').should('contain', 'all_factions by unknown');
+    cy.get('head title').should(
+      'contain',
+      'all_factions_three by commonAccount'
+    );
     cy.get('head meta[name="description"]')
       .invoke('attr', 'content')
       .should('contain', 'Gauntlet Mythgard Deck, YGPBRO, 12750 Essence');
     cy.get('head meta[property="og:title"]')
       .invoke('attr', 'content')
-      .should('contain', 'all_factions by unknown');
+      .should('contain', 'all_factions_three by commonAccount');
     cy.get('head meta[property="og:description"]')
       .invoke('attr', 'content')
       .should('contain', 'Gauntlet Mythgard Deck, YGPBRO, 12750 Essence');
 
     // Page elements
     cy.get('[data-cy="editMetaValue"]').should('not.be.visible');
+    cy.get('[data-cy="userAvatar"]').should('have.length', 1);
     cy.get('[data-cy="userLink"]').should('not.be.visible');
     cy.get('[data-cy="deckPageType"]').should('be.visible');
     cy.get('[data-cy="deckPageArchetype"]').should('be.visible');
