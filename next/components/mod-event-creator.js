297c644494fe35da31f3eeb7b4b7447c6b5ea163
alexMA <almiddeleer@gmail.com>
2020-11-08 16:10:46 -0500

    Add other fields

--- next/components/mod-event-creator.js
+++ next/components/mod-event-creator.js
@@ -6,10 +6,12 @@ import useCreateEventMutation from '../lib/use-create-event-mutation';
 function modEventCreator() {
   const [name, setName] = useState('My cool event');
   const onChangeName = handleInputChangeHooks(setName);
-
-  const organizer = 'org';
-  const url = 'http://www.mythardhub.com';
-  const date = '2000-01-01';
+  const [organizer, setOrganizer] = useState('organizer');
+  const onChangeOrganizer = handleInputChangeHooks(setOrganizer);
+  const [url, setUrl] = useState('https://www.mythgardhub.com/');
+  const onChangeUrl = handleInputChangeHooks(setUrl);
+  const [date, setDate] = useState('2000-01-01');
+  const onChangeDate = handleInputChangeHooks(setDate);
 
   const [createEventMutation] = useCreateEventMutation();
   const createEvent = () =>
@@ -17,9 +19,26 @@ function modEventCreator() {
 
   return (
     <div>
+      <style jsx>{`
+        label {
+          display: inline-block;
+          margin: 0px 20px 20px;
+          white-space: nowrap;
+        }
+      `}</style>
       <h1>Create Event</h1>
       <label>
         Name: <input type="text" value={name} onChange={onChangeName} />
+      </label>
+      <label>
+        Organizer:{' '}
+        <input type="text" value={organizer} onChange={onChangeOrganizer} />
+      </label>
+      <label>
+        Url: <input type="text" value={url} onChange={onChangeUrl} />
+      </label>
+      <label>
+        Date: <input type="text" value={date} onChange={onChangeDate} />
       </label>
       <br />
       <button style={{ width: 100 }} onClick={() => createEvent()}>
