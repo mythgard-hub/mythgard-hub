3695b96b5c22f59388c849f5e1827775ea0b0569
alexMA <almiddeleer@gmail.com>
2020-11-14 16:58:53 -0500

    Load all events

--- next/components/mod-edit-events.js
+++ next/components/mod-edit-events.js
@@ -2,8 +2,11 @@ import { useState } from 'react';
 import { handleInputChangeHooks } from '../lib/form-utils.js';
 import { formatDate } from '../lib/graphql-utils.js';
 import useCreateEventMutation from '../lib/use-create-event-mutation';
+import { useQuery } from '@apollo/react-hooks';
+import { allTournaments } from '../lib/tournament-queries.js';
 
 function modEditEvents() {
+  const { loading, error, data } = useQuery(allTournaments);
   // const [name, setName] = useState('My cool event');
   // const onChangeName = handleInputChangeHooks(setName);
   // const [organizer, setOrganizer] = useState('organizer');
@@ -16,11 +19,13 @@ function modEditEvents() {
   // const [createEventMutation] = useCreateEventMutation();
   // const createEvent = () =>
   //   createEventMutation({ variables: { name, url, organizer, date } });
+  //
 
   return (
     <div>
       <style jsx>{``}</style>
       <h2>Event results</h2>
+      {JSON.stringify(data)}
     </div>
   );
 }
