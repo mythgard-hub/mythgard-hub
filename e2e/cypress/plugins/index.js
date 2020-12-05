590aa3235dae66957e2e23c05d5dc4761968554f
alexMA <almiddeleer@gmail.com>
2020-12-05 12:32:34 -0500

    make cypress fail fast

--- e2e/cypress/plugins/index.js
+++ e2e/cypress/plugins/index.js
@@ -12,6 +12,6 @@
 // the project's config changing)
 
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+  require('cypress-fail-fast/plugin')(on, config);
+  return config;
+};
